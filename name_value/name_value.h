4d518d885f815583b9bd06fa7f640a77bf5b79d9
seva <vlaskine@yahoo.com>
2012-10-25 14:54:43 +1100

    comma-config.cmake.in renamed to comma_config.cmake.in

---
+++ name_value/name_value.h
@@ -0,0 +1,26 @@
+// This file is part of comma, a generic and flexible library 
+// for robotics research.
+//
+// Copyright (C) 2011 The University of Sydney
+//
+// comma is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// comma is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with comma. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef COMMA_NAME_VALUE_NAME_VALUE_H_
+#define COMMA_NAME_VALUE_NAME_VALUE_H_
+
+#include <comma/name_value/map.h>
+#include <comma/name_value/parser.h>
+#include <comma/name_value/ptree.h>
+
+#endif // #ifndef COMMA_NAME_VALUE_NAME_VALUE_H_
