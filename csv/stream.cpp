daf8d0240331df20b38d793366a102d96a369d5f
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2014-03-07 16:13:48 +1100

    csv/stream: std::cin is untied from std::cout in static space now, since otherwise it is not thread-safe anymore (because of sync_with_stdio( false )

--- csv/stream.cpp
+++ csv/stream.cpp
@@ -2,8 +2,18 @@
 
 namespace comma { namespace csv { namespace detail {
 
-static const bool unsyncronize_with_stdio_dummy = std::ios_base::sync_with_stdio( false );
+bool unsyncronize_with_stdio_impl()
+{
+    bool b = std::ios_base::sync_with_stdio( false ); // std::cin, std::cout access are thread-unsafe now (safe by default)
+    std::cin.tie( NULL ); // std::cin is tied to std::cout by default, which is thread-unsafe now
+}
 
-void unsyncronize_with_stdio() { static bool dummy = unsyncronize_with_stdio_dummy; ( void )( dummy ); } // necessary, otherwise linker would not link to dummy symbol
+static const bool unsyncronize_with_stdio_dummy = unsyncronize_with_stdio_impl();
+
+void unsyncronize_with_stdio()
+{
+    static bool dummy = unsyncronize_with_stdio_dummy;
+    ( void )( dummy ); // necessary, otherwise linker would not link to dummy symbol
+}
 
 } } } // namespace comma { namespace csv { namespace detail {
