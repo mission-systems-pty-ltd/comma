ee003c920c8e12e364251830233f5d0450ef97eb
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2014-06-06 08:57:15 +1000

    name_value/to_xml: a poor man's xml serializing visitor implemented

---
+++ name_value/to_xml.h
@@ -0,0 +1,97 @@
+#ifndef COMMA_XML_TO_XML_
+#define COMMA_XML_TO_XML_
+
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/type_traits.hpp>
+#include <comma/visiting/while.h>
+
+namespace comma {
+
+/// a simple visitor for outputting an xml fragment (which is not possible with boost::property_tree)
+class to_xml
+{
+    public:
+        to_xml( std::ostream& os, unsigned int indent_step = 4, unsigned int indent = 0 ) : os_( os ), indent_( indent, ' ' ), indent_step_( indent_step, ' ' ), final_( false ), closed_( true ) {}
+
+        /// apply_next on boost optional
+        template < typename K, typename T >
+        void apply_next( const K& name, const boost::optional< T >& value )
+        {
+            if( !value ) { return; }
+            visiting::do_while<    !boost::is_fundamental< T >::value
+                                && !boost::is_same< T, boost::posix_time::ptime >::value
+                                && !boost::is_same< T, std::string >::value >::visit( name, *value, *this );
+        }
+
+        /// apply
+        template < typename K, typename T, typename A >
+        void apply( const K& name, const std::vector< T, A >& value )
+        {
+            for( unsigned int i = 0; i < value.size(); ++i )
+            {
+                open_( name );
+                visiting::do_while<    !boost::is_fundamental< T >::value
+                                    && !boost::is_same< T, boost::posix_time::ptime >::value
+                                    && !boost::is_same< T, std::string >::value >::visit( i, value[i], *this );
+                close_( name );
+            }
+        }
+
+        /// apply
+        template < typename K, typename T >
+        void apply( const K& name, const T& value )
+        {
+            std::string s = boost::lexical_cast< std::string >( name );
+            open_( &s[0] );
+            visiting::do_while<    !boost::is_fundamental< T >::value
+                                && !boost::is_same< T, boost::posix_time::ptime >::value
+                                && !boost::is_same< T, std::string >::value >::visit( name, value, *this );
+            close_( &s[0] );
+        }
+
+        /// apply to non-leaf elements
+        template < typename K, typename T >
+        void apply_next( const K& name, const T& value ) { comma::visiting::visit( name, value, *this ); }
+
+        /// apply to leaf elements
+        template < typename K, typename T >
+        void apply_final( const K& name, const T& value )
+        {
+            os_ << value_( value );
+            final_ = true;
+        }
+
+    private:
+        std::ostream& os_;
+        std::string indent_;
+        std::string indent_step_;
+        bool final_;
+        bool closed_;
+        void open_( const char* name )
+        {
+            if( name[0] == '\0' ) { return; }
+            if( !indent_step_.empty() && !closed_ ) { os_ << std::endl; }
+            os_ << indent_ << "<" << name << ">";
+            if( indent_step_.empty() ) { return; }
+            indent_ += indent_step_;
+            closed_ = false;
+        }
+        void close_( const char* name )
+        {
+            if( name[0] == '\0' ) { return; }
+            indent_ = indent_.substr( 0, indent_.size() - indent_step_.size() );
+            if( !final_ ) { os_ << indent_; }
+            final_ = false;
+            os_ << "</" << name << ">";
+            if( !indent_step_.empty() ) { os_ << std::endl; }
+            closed_ = true;
+        }
+        static std::string value_( const boost::posix_time::ptime& t ) { return boost::posix_time::to_iso_string( t ); }
+        template < typename T > static T value_( T v ) { return v; }
+};
+
+} // namespace comma {
+
+#endif
