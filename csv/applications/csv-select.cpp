3eb73aa8a1819c794d8e85581d3e738b138d9313
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2014-06-17 15:30:07 +1000

    csv-select: reviewed, minor refactoring; --help for --format: todo

--- csv/applications/csv-select.cpp
+++ csv/applications/csv-select.cpp
@@ -386,16 +386,9 @@ int main( int ac, char** av )
                 if( !line.empty() ) { break; }
             }
             if( line.empty() ) { return 0; }
-
-            comma::csv::format format;
-            if( options.exists( "--format" ) )
-            {
-                format = comma::csv::format( options.value< std::string >( "--format" ) );
-            }
-            else
-            {
-                format = guess_format( line );
-            }
+            comma::csv::format format = options.exists( "--format" )
+                                      ? comma::csv::format( options.value< std::string >( "--format" ) )
+                                      : guess_format( line );
             init_input( format, options );
             comma::csv::ascii_input_stream< input_t > istream( std::cin, csv, input );
             // todo: quick and dirty: no time to debug why the commented section does not work (but that's the right way)
