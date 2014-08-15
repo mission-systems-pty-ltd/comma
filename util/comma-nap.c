050d96739c1868f751d9bb00ef724f1d912c72d5
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2014-08-15 11:43:04 +1000

    Better formatting for the help message.

--- util/comma-nap.c
+++ util/comma-nap.c
@@ -5,11 +5,12 @@
 
 int main(int argc, char *argv[]) {
     if ( argc < 2 ) {
-        fprintf(stderr, "Usage: %s <seconds to sleep>\n", argv[0]);
-        fprintf(stderr, "       %s is a verbose replacement of sleep (2) utility\n", argv[0]);
-        fprintf(stderr, "       %*c will print a message at the end of the run, if terminates\n", strlen(argv[0]), ' ');
-        fprintf(stderr, "       %*c normally, and stay silent if killed.\n", strlen(argv[0]), ' ');
-        fprintf(stderr, "       %*c Input is an integer number of seconds to sleep.\n", strlen(argv[0]), ' ');
+        fprintf(stderr, "Usage: %s <seconds to sleep>\n\n", argv[0]);
+        int alen = (int)strlen(argv[0]);
+        fprintf(stderr, "%s is a verbose replacement of sleep (2) utility\n", argv[0]);
+        fprintf(stderr, "%*c It will print a message at the end of the run, if terminates\n", alen, ' ');
+        fprintf(stderr, "%*c normally, and stay silent if killed.\n", alen, ' ');
+        fprintf(stderr, "%*c Input is an integer number of seconds to sleep.\n", alen, ' ');
         return 1;
     }
     int d = atoi( argv[1] );
