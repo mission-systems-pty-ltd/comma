edfe3c621d81e2d0a4c6a20dc4c5cb9dac7fa8fd
m-hounsell-acfr <m.hounsell@acfr.usyd.edu.au>
2014-09-05 13:49:50 +1000

    xml-split: it is now finding files

--- xpath/applications/xml-split.cpp
+++ xpath/applications/xml-split.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <limits>
 #include <list>
+#include <set>
 
 #include <expat.h>
 
@@ -25,8 +26,11 @@ static unsigned element_depth_max = 0;
 static XML_Parser parser = NULL;
 
 // can't use list or map on xpath because of the < overloading
-static std::list<comma::xpath> exact_list;
+static std::set<comma::xpath, comma::xpath::less_t> exact_set;
 static std::list<comma::xpath> grep_list;
+
+static std::list<comma::xpath> element_path_list;
+static unsigned element_found = 0;
 
 // ~~~~~~~~~~~~~~~~~~
 // UTILITIES
@@ -37,8 +41,41 @@ namespace comma
     std::ostream &
     operator <<(std::ostream & os, comma::xpath const & p)
     {
-        return p.output(os);
+        return p.output(os, '|');
     }
+}
+
+bool
+grep(XML_Char const * element, comma::xpath const & element_path)
+{
+    // std::cerr << element << " or " << element_path << std::endl;
+
+    if (exact_set.end() != exact_set.find(element_path))
+    {
+        std::cerr << "Found Exact " << element_path << std::endl;
+        return true;
+    }
+    /*
+    std::list<comma::xpath>::const_iterator const exact_result
+        = std::find(exact_list.begin(), exact_list.end(), element_path);
+    if (exact_list.end() != exact_result)
+        return true;
+    */
+
+    std::list<comma::xpath>::const_iterator const end = grep_list.end();
+    std::list<comma::xpath>::const_iterator itr = grep_list.begin();
+    
+    for (; itr != end; ++itr)
+    {
+        // std::cerr << "Compare " << element << " Against  " << itr->elements.back().name << std::endl;
+        if (itr->elements.back().name == element)
+        {
+            // std::cerr << "Found Partial " << element_path << std::endl;
+            return true;
+        }
+    }
+
+    return false;
 }
 
 // ~~~~~~~~~~~~~~~~~~
@@ -60,6 +97,22 @@ usage(bool const verbose)
 // SAX HANDLERS
 // ~~~~~~~~~~~~~~~~~~
 static void XMLCALL
+default_handler(void * userdata, XML_Char const * str, int length)
+{
+    assert(NULL != str);
+    assert(length > 0);
+
+    if (element_found > 0)
+        std::cout.write(str, length);
+}
+
+static void XMLCALL
+comment(void * userdata, XML_Char const * str)
+{
+    // DO NOTHING
+}
+
+static void XMLCALL
 element_start(void * userdata, char const * element, char const ** attributes)
 {
     assert(NULL != element);
@@ -69,6 +122,36 @@ element_start(void * userdata, char const * element, char const ** attributes)
     ++element_count;
     element_depth_max = std::max(element_depth_max, element_depth);
     ++element_depth;
+    
+    // build the xpath
+    comma::xpath element_path;
+    if (! element_path_list.empty())
+        element_path = element_path_list.back();
+    element_path /= std::string(element);
+    element_path_list.push_back(element_path);
+    
+    if (grep(element, element_path))
+    {
+        ++element_found_count;
+        ++element_found;
+    }
+
+    if (element_found > 0)
+    {
+        std::cout  << '<' << element << ' ';
+        if (NULL != attributes)
+            for (unsigned i = 0; NULL != attributes[i]; i += 2)
+            {
+                std::cout << attributes[i] << "='";
+                for (XML_Char const * p = attributes[i+1]; *p != 0; ++p)
+                    if ('\'' == *p)
+                        std::cout << "&apos;";
+                    else
+                        std::cout << *p;
+                std::cout << "' ";
+            }
+        std::cout << '>';
+    }
 }
 
 static void
@@ -76,6 +159,19 @@ element_end(void * userdata, char const * element)
 {
     assert(NULL != element);
 
+    comma::xpath const & element_path = element_path_list.back();
+    bool const was_found = element_found > 0;
+    
+    if (was_found)
+        std::cout << "</" << element << '>';
+    
+    if (grep(element, element_path))
+        --element_found;
+        
+    if (was_found && 0 == element_found)
+        std::cout << std::endl;
+
+    element_path_list.pop_back();
     --element_depth;
 }
 
@@ -129,6 +225,8 @@ run()
     }
 
     XML_SetElementHandler(parser, element_start, element_end);
+    XML_SetDefaultHandler(parser, default_handler);
+    XML_SetCommentHandler(parser, comment);
 
     bool ok = false;
     if (options_file.empty())
@@ -182,21 +280,26 @@ int main(int argc, char ** argv)
 
         for (unsigned i = 1; i < unsigned(argc); ++i)
         {   
+            if ('-' == argv[i][0])
+                continue;
+            
             //std::string const str(argv[i]);
             if (0 == argv[i][1]) // length of 1
-                exact_list.push_back(comma::xpath(argv[i]));
+                exact_set.insert(comma::xpath(argv[i]));
             else if ('/' != argv[i][0])
                 grep_list.push_back(comma::xpath(argv[i]));
             else if ('/' != argv[i][1])
-                exact_list.push_back(comma::xpath(argv[i]));
+                exact_set.insert(comma::xpath(argv[i] + 1));
             else 
                 grep_list.push_back(comma::xpath(argv[i] + 2));
         }
 
-        if (false)
+        if (true)
         {
             std::ostream_iterator<comma::xpath const> out_itr(std::cerr, "\n");
-            std::copy(exact_list.begin(), exact_list.end(), out_itr);
+            std::cerr << "Exacts ..." << std::endl;
+            std::copy(exact_set.begin(), exact_set.end(), out_itr);
+            std::cerr << "Partials ..." << std::endl;
             std::copy(grep_list.begin(), grep_list.end(), out_itr);
         }
         
