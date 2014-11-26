f2bf6cb178a5668ff71cb156f714bdb20706f984
Andrey Sokolov <a.sokolov@acfr.udyd.edu.au>
2014-11-26 14:50:11 +1100

    comma/base/types.h: added integer type

--- base/types.h
+++ base/types.h
@@ -82,6 +82,14 @@ BOOST_STATIC_ASSERT( sizeof( int64 ) == 8 );
 
 #endif
 
+template < unsigned int Size, bool Signed = true > struct integer {};
+template <> struct integer< 2, true > { typedef comma::int16 type; };
+template <> struct integer< 2, false > { typedef comma::uint16 type; };
+template <> struct integer< 4, true > { typedef comma::int32 type; };
+template <> struct integer< 4, false > { typedef comma::uint32 type; };
+template <> struct integer< 8, true > { typedef comma::int64 type; };
+template <> struct integer< 8, false > { typedef comma::uint64 type; };
+
 } // namespace comma {
 
 #endif /*COMMA_BASE_TYPES_H_*/
