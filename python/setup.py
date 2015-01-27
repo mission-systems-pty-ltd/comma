71ce7795bb1b523d23853809cb1ce99ade6d63bf
v-do <vdo.code@gmail.com>
2015-01-27 11:36:11 +1100

    python: install python modules and scripts

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name                = 'comma',
+        version             = '1.0',
+        description         = 'comma python utilties',
+        url                 = 'https://github.com/acfr/comma',
+        license             = 'BSD 3-Clause',
+        packages            = [ 'comma', 'comma.csv', 'comma.io' ],
+     )
