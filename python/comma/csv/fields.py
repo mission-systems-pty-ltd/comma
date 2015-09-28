432effca957a28dd7df3d137420d2887b2ed61a0
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2015-09-28 12:20:51 +1000

    python/comma/csv/fields.py: accept fields and format not matching struct type

--- python/comma/csv/fields.py
+++ python/comma/csv/fields.py
@@ -27,22 +27,46 @@ class struct:
         if what == 'fields': items.append( path + name )
         elif what == 'format': items.append( dtype.name )
     return items
-    
+
 class stream:
-  def __init__( self, struct, fields=None, format=None, delimiter=',', flush=False ):
+  def __init__( self, struct, fields=None, format=None, flush=False ):
     self.struct = struct
     self.fields = fields if fields else struct.fields
     self.format = format if format else struct.format
+    self.dtype = numpy.dtype( self.format )
     self.flush = flush
-  
-  def read( self, size=1, recarray=True ):
+    if self.fields != struct.fields:
+      names = map( lambda _: 'f' + str( self.fields.split(',').index( _ ) ), struct.fields.split(',') )
+      formats = [ self.dtype.fields[name][0] for name in names ]
+      offsets = [ self.dtype.fields[name][1] for name in names ]
+      self.reshaped_dtype = numpy.dtype( dict( names=names, formats=formats, offsets=offsets ) )
+      #print >> sys.stderr, "init: names: ", names
+      #print >> sys.stderr, "init: formats: ", formats
+      #print >> sys.stderr, "init: offsets: ", offsets
+      #print >> sys.stderr, "init: dtype: ", self.dtype
+      #print >> sys.stderr, "init: reshaped_dtype: ", self.reshaped_dtype
+
+  def iter( self, size=1, recarray=True  ):
     while True:
-      data = numpy.fromfile( sys.stdin, dtype=self.format, count=size )
-      if data.size == 0: break
-      s = data.view( self.struct ) # TODO: implement visiting for when format has more fields or they are in different order
-      yield s if not recarray else s.view( numpy.recarray )
-    
+      s = self.read( size, recarray )
+      if s is None: break
+      yield s
+
+  def read( self, size=1, recarray=True ):
+    data = numpy.fromfile( sys.stdin, dtype=self.format, count=size )
+    if data.size == 0: return None
+    #print >> sys.stderr, "data.dtype: ", data.dtype
+    #print >> sys.stderr, "self.dtype: ", self.dtype
+    #print >> sys.stderr, "self.reshaped_dtype: ", self.reshaped_dtype
+    #print >> sys.stderr, "self.struct.dtype: ", self.struct.dtype
+    #print >> sys.stderr, "self.struct.format: ", self.struct.format
+    #print >> sys.stderr, "data: ", data
+    #print >> sys.stderr, "ndarray: ", numpy.ndarray( data.shape, self.reshaped_dtype, data )[0]
+    #print >> sys.stderr, "tuple: ", tuple( numpy.ndarray( data.shape, self.reshaped_dtype, data )[0] )
+    #print >> sys.stderr, "array: ", numpy.array( tuple( numpy.ndarray( data.shape, self.reshaped_dtype, data )[0] ), dtype=self.struct.format )
+    s = numpy.array( tuple( numpy.ndarray( data.shape, self.reshaped_dtype, data )[0] ), dtype=self.struct.format ).view( self.struct ) if self.reshaped_dtype else data.view( self.struct )
+    return s.view( numpy.recarray ) if recarray else s
+
   def write( self, s ):
     if s.dtype == self.struct: s.tofile( sys.stdout )
     if self.flush: sys.stdout.flush()
-    
