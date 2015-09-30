50e4eabfe2dcc7068fcb08023bc31fc8e480f699
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2015-09-30 16:20:39 +1000

    python/comma/csv/fields.py: ascii input/output conforms to comma format conventions

--- python/comma/csv/fields.py
+++ python/comma/csv/fields.py
@@ -6,6 +6,8 @@ from StringIO import StringIO
 import itertools
 import io
 import warnings
+import operator
+import re
 
 class struct:
   def __init__( self, fields, *types ):
@@ -25,8 +27,27 @@ class struct:
         elif what == 'format': items.append( dtype.str )
     return items
 
+def get_numpy_format( comma_format ):
+  numpy_types_dict = dict( b='int8', ub='uint8', w='int16', uw='uint16', i='int32', ui='uint32', l='int64', ul='uint64', f='float32', d='float64', t='datetime64[us]' )
+  def numpy_string_type( comma_string_type ):
+    match = re.match( '^s\[(\d+)\]$', comma_string_type )
+    if match: return 'S' + match.group(1)
+  numpy_types = []
+  for comma_type in comma_format.split(','):
+    numpy_type = numpy_types_dict.get( comma_type ) or numpy_string_type( comma_type )
+    if numpy_type is None: raise Exception( "format string '{}' contains unrecongnised type '{}'".format( comma_format, comma_type ) )
+    numpy_types.append( numpy_type )
+  return ','.join( numpy_types )
+
+def get_numpy_time( comma_time_string ):
+  v = list( comma_time_string )
+  if len( v ) < 15: raise Exception( "ascii time string '{}' is not recognised".format( comma_time_string ) )
+  for i in [13,11]: v.insert( i, ':' )
+  for i in [6,4]: v.insert( i, '-' )
+  return numpy.datetime64( ''.join( v ), 'us' )
+
 class stream:
-  def __init__( self, struct, fields=None, format=None, binary=False, delimiter=',', flush=False ):
+  def __init__( self, struct, fields=None, format=None, binary=False, delimiter=',', flush=False, is_numpy_format=False ):
     self.struct = struct
     self.fields = fields if fields else self.struct.fields
     self.binary = binary or format is not None
@@ -34,12 +55,16 @@ class stream:
     self.flush = flush
     if format is None:
       if self.fields == self.struct.fields:
-        format = self.struct.format
+        self.format = self.struct.format
       else:
         struct_format_of_field = dict( zip( self.struct.fields.split(','), self.struct.format.split(',') ) )
-        format = ','.join( struct_format_of_field.get( name ) or 'S' for name in self.fields.split(',') )
-    self.dtype = numpy.dtype( format )
+        self.format = ','.join( struct_format_of_field.get( name ) or 'S' for name in self.fields.split(',') )
+    else:
+      self.format = format if is_numpy_format else get_numpy_format( format )
+    self.dtype = numpy.dtype( self.format )
     self.default_size = max( 1, 65536 / self.dtype.itemsize )
+    if not self.binary:
+      self.converters = { i:get_numpy_time for i in numpy.where( numpy.array( self.format.split(',') ) == numpy.dtype('datetime64[us]').str )[0] }
     self.struct_dtype = numpy.dtype( self.struct.format )
     if self.fields == self.struct.fields:
         self.reshaped_dtype = None
@@ -62,11 +87,7 @@ class stream:
     else:
       with warnings.catch_warnings():
         warnings.simplefilter( 'ignore' )
-        data = numpy.loadtxt( StringIO( ''.join( itertools.islice( sys.stdin, size ) ) ), dtype=self.dtype , delimiter=self.delimiter )
-      names = map( lambda _: 'f' + str( self.fields.split(',').index( _ ) ), self.struct.fields.split(',') )
-      formats = [ data.dtype.fields[name][0] for name in names ]
-      offsets = [ data.dtype.fields[name][1] for name in names ]
-      self.reshaped_dtype = numpy.dtype( dict( names=names, formats=formats, offsets=offsets ) )
+        data = numpy.loadtxt( StringIO( ''.join( itertools.islice( sys.stdin, size ) ) ), dtype=self.dtype , delimiter=self.delimiter, converters=self.converters )
     if data.size == 0: return None
     s = numpy.array( map( tuple, numpy.ndarray( data.shape, self.reshaped_dtype, data )[:] ), dtype=self.struct_dtype ).view( self.struct ) if self.reshaped_dtype else data.view( self.struct )
     return s.view( numpy.recarray ) if recarray else s
@@ -75,6 +96,7 @@ class stream:
     if self.binary:
       s.tofile( sys.stdout )
     else:
+      to_string = lambda _: _.item().isoformat().translate( None, '-:' ) if isinstance( _, numpy.datetime64 ) else str( _.item() )
       for _ in s.view( self.struct_dtype ):
-        print ','.join( map( str, _ ) )
+        print ','.join( map( to_string, _ ) )
     if self.flush: sys.stdout.flush()
