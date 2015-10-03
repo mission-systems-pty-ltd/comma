019ddededa75ab66b52163adab0d9ef510d0d877
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2015-10-04 09:41:45 +1100

    python/comma/csv: added capability to process arrays as elements of dtype

--- python/comma/csv/__init__.py
+++ python/comma/csv/__init__.py
@@ -10,56 +10,56 @@ import types
 class struct:
   def __init__( self, fields, *types ):
     if len( fields.split(',') ) != len( types ):
-      raise Exception( "expected {} types for fields '{}', got {} type(s)".format( len( fields.split(',') ), fields, len( types )) )
+      raise Exception( "expected {} types for '{}', got {} type(s)".format( len( fields.split(',') ), fields, len( types )) )
     self.dtype = numpy.dtype( zip( fields.split(','), types ) )
-    self.fields = ','.join( struct.get( 'fields', self.dtype ) )
-    self.format = ','.join( struct.get( 'format', self.dtype ) )
+    self.fields = struct.get( 'fields', self.dtype )
+    self.types = struct.get( 'types', self.dtype )
 
   @staticmethod
-  def get( what, nested_dtype, path='' ):
+  def get( what, parent_dtype, path='' ):
     items = []
-    for name in nested_dtype.names:
-      dtype = nested_dtype.fields[name][0]
-      if dtype.type == numpy.void and not dtype.subdtype:
-        items.extend( struct.get( what, dtype, path + name + '/' ) )
+    for name in parent_dtype.names:
+      child_dtype = parent_dtype.fields[name][0]
+      if child_dtype.type == numpy.void and not child_dtype.subdtype:
+        items.extend( struct.get( what, child_dtype, path + name + '/' ) )
       else:
         if what == 'fields': items.append( path + name )
-        elif what == 'format': items.append( dtype.str )
-    return items
+        elif what == 'types': items.append( child_dtype.str )
+    return tuple( items )
 
 class stream:
   buffer_size_in_bytes = 65536
   def __init__( self, s, fields=None, format=None, binary=False, delimiter=',', flush=False ):
     if not isinstance( s, struct ): raise Exception( "expected '{}', got '{}'".format( str( struct ), repr( s ) ) )
     self.struct = s
-    if fields is None:
-      self.fields = self.struct.fields
+    if fields:
+      if not set( fields.split(',') ).issuperset( self.struct.fields ):
+        unaccounted_fields = set( self.struct.fields ) - set( fields.split(',') )
+        raise Exception( "expected field(s) '{}' not found in supplied fields '{}'".format( ','.join( unaccounted_fields ), fields ) )
+      self.fields = tuple( fields.split(',') )
     else:
-      for field in self.struct.fields.split(','):
-        if not field in fields.split(','): raise Exception( "expected field '{}' is not found in fields '{}'".format( field, fields ) )
-      self.fields = fields
+      self.fields = self.struct.fields
     self.binary = binary or format is not None
     self.delimiter = delimiter if not self.binary else None
     self.flush = flush
-    if format is None:
-      if self.fields == self.struct.fields:
-        self.format = self.struct.format
-      else:
-        struct_format_of_field = dict( zip( self.struct.fields.split(','), self.struct.format.split(',') ) )
-        self.format = ','.join( struct_format_of_field.get( name ) or 'S' for name in self.fields.split(',') )
+    if format:
+      self.dtype = numpy.dtype( format )
     else:
-      if len( self.fields.split(',') ) != len( format.split(',') ):
-        raise Exception( "expected same number of fields and format types, got '{}' and '{}'".format( self.fields, format ) )
-      self.format = format
-    self.dtype = numpy.dtype( self.format )
+      if self.fields == self.struct.fields:
+        self.dtype = numpy.dtype( ','.join( self.struct.types ) )
+      else:
+        struct_type_of_field = dict( zip( self.struct.fields, self.struct.types ) )
+        self.dtype = numpy.dtype( ','.join( struct_type_of_field.get( name ) or 'S' for name in self.fields ) )
+    if format and len( self.fields ) != len( self.dtype.names ):
+      raise Exception( "expected same number of fields and format types, got '{}' and '{}'".format( self.fields, format ) )    
     self.size = max( 1, stream.buffer_size_in_bytes / self.dtype.itemsize )
     if not self.binary:
-      self.converters = { i: types.time_to_numpy for i in numpy.where( numpy.array( self.format.split(',') ) == numpy.dtype('datetime64[us]').str )[0] }
-    self.struct_flat_dtype = numpy.dtype( self.struct.format )
+      self.converters = { index: types.time_to_numpy for index in numpy.where( numpy.array( struct.get( 'types', self.dtype ) ) == numpy.dtype('datetime64[us]').str )[0] }
+    self.struct_flat_dtype = numpy.dtype( ','.join( self.struct.types ) )
     if self.fields == self.struct.fields:
         self.reshaped_dtype = None
     else:
-      names = map( lambda _: 'f' + str( self.fields.split(',').index( _ ) ), self.struct.fields.split(',') )
+      names = map( lambda _: 'f' + str( self.fields.index( _ ) ), self.struct.fields )
       formats = [ self.dtype.fields[name][0] for name in names ]
       offsets = [ self.dtype.fields[name][1] for name in names ]
       self.reshaped_dtype = numpy.dtype( dict( names=names, formats=formats, offsets=offsets ) )
