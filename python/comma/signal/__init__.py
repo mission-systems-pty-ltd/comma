bc0e6c0ca109da280e37e82fb0951b174a6dc702
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-02-09 17:14:33 +1100

    python: add signal handling (needs testing)

---
+++ python/comma/signal/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+import signal
+import sys
+
+MANAGED_SIGNALS = tuple( [ signal.SIGINT, signal.SIGTERM, signal.SIGPIPE, signal.SIGHUP ] )
+
+class is_shutdown:
+  def __init__( self ):
+    self.state = False
+    for s in MANAGED_SIGNALS:
+      signal.signal( s, self.switch_on )
+
+  def switch_on( self, signum, frame ):
+    print >> sys.stderr, "caught signal: ", signum
+    self.state = True
+
+  def __nonzero__( self ):
+    return self.state
