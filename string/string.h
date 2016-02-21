808f8ca15ab67c2ad767ba923132ab4304724733
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2016-02-22 10:30:16 +1100

    No need to demand random-access iterators in the join method. Forward iterators suffice just fine. Add a unit test to prove the implementation.

--- string/string.h
+++ string/string.h
@@ -70,8 +70,9 @@ inline std::string join( const A& a, std::size_t size, char delimiter )
 {
     if( size == 0 ) { return ""; }
     std::ostringstream oss;
-    oss << a[0];
-    for( std::size_t i = 1; i < size; ++i ) { oss << delimiter << a[i]; }
+    oss << a.front();
+    typename A::const_iterator current = a.begin();
+    while (++current != a.end() ) { oss << delimiter << *current; }
     return oss.str();
 }
 
