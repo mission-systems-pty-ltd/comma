5a930247ea2954aab26d76ab842ddadd4d0aba72
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-03-26 21:27:11 +1100

    python/comma/csv/applications/csv-eval: improve help and add examples (strings and time)

--- python/comma/csv/applications/csv_eval.py
+++ python/comma/csv/applications/csv_eval.py
@@ -67,7 +67,7 @@ def evaluate( expressions, stream, dangerous=False ):
 def add_csv_options( parser ):
   comma.csv.options.standard_csv_options( parser, { 'fields': 'x,y,z' } )
   
-  parser.add_argument( "--append-fields", "-F", help="output fields appended to input stream (by default, inferred from expressions)", metavar='<names>' )
+  parser.add_argument( "--append-fields", "-F", help="fields appended to input stream (by default, inferred from expressions)", metavar='<names>' )
   parser.add_argument( "--append-binary", "-B", help="for binary stream, binary format of appended fields (by default, 'd' for each)", metavar='<format>' )
 
 def main():
@@ -76,34 +76,41 @@ evaluate numerical expressions and append computed values to csv stream
 """
   epilog="""
 notes:
-    1) in ascii mode, specified input fields are treated as floating point numbers
-    2) output fields appended to input stream are inferred from expressions (by default) or specified by --append-fields
+    1) in ascii mode, input fields are treated as floating point numbers
+    2) fields appended to input stream are inferred from expressions (by default) or specified by --append-fields
     3) if --append-fields is omitted, only simple assignment statements are allowed in expressions
-    4) in binary mode, appended fields are assigned type 'd' (by default) or format is specified by --append-binary
+    4) in binary mode, appended fields are assigned comma type 'd' (by default) or format is specified by --append-binary
+    5) full xpath input fields are not allowed
 
 examples:
     ( echo 1,2; echo 3,4 ) | {script_name} --fields=x,y --precision=2 'a=2/(x+y);b=x-sin(y)*a**2'
-    ( echo 1,2; echo 3,4 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'a=2/(x+y);b=x-sin(y)*a**2' -v | csv-from-bin 4d
+    ( echo 1,2; echo 3,4 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'a=2/(x+y);b=x-sin(y)*a**2' | csv-from-bin 4d
 
     # define intermediate variable
-    ( echo 1; echo 2 ) | csv-to-bin d | {script_name} --binary=d --fields=x 'a=2;y=a*x' --append-fields=y -v | csv-from-bin 2d
+    ( echo 1; echo 2 ) | csv-to-bin d | {script_name} --binary=d --fields=x 'a=2;y=a*x' --append-fields=y | csv-from-bin 2d
 
     # take minimum
-    ( echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'c=minimum(x,y)' -v | csv-from-bin 3d
+    ( echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'c=minimum(x,y)' | csv-from-bin 3d
 
     # clip index
-    ( echo a,2; echo b,5 ) | csv-to-bin s[1],ui | {script_name} --binary=s[1],ui --fields=,id 'i=clip(id,3,inf)' --append-binary=ui -v | csv-from-bin s[1],ui,ui
+    ( echo a,2; echo b,5 ) | csv-to-bin s[1],ui | {script_name} --binary=s[1],ui --fields=,id 'i=clip(id,3,inf)' --append-binary=ui | csv-from-bin s[1],ui,ui
 
     # compare fields
-    ( echo 1,2; echo 4,3 ) | csv-to-bin 2i | {script_name} --binary=2i --fields=i,j 'flag=i+1==j' --append-binary=b -v | csv-from-bin 2i,b
-    ( echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'flag=x<y' --append-binary=b -v | csv-from-bin 2d,b
-    ( echo 0,1; echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'flag=logical_and(x<y,y<2)' --append-binary=b -v | csv-from-bin 2d,b
+    ( echo 1,2; echo 4,3 ) | csv-to-bin 2i | {script_name} --binary=2i --fields=i,j 'flag=i+1==j' --append-binary=b | csv-from-bin 2i,b
+    ( echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'flag=x<y' --append-binary=b | csv-from-bin 2d,b
+    ( echo 0,1; echo 1,2; echo 4,3 ) | csv-to-bin 2d | {script_name} --binary=2d --fields=x,y 'flag=logical_and(x<y,y<2)' --append-binary=b | csv-from-bin 2d,b
 
     # negate boolean
-    ( echo 0; echo 1 ) | csv-to-bin b | {script_name} --binary=b --fields=flag 'a=logical_not(flag)' --append-binary=b -v | csv-from-bin 2b
+    ( echo 0; echo 1 ) | csv-to-bin b | {script_name} --binary=b --fields=flag 'a=logical_not(flag)' --append-binary=b | csv-from-bin 2b
 
-    # select operation based on condition ( 'where(x<y,x+y,x-y)' is equivalent to 'x<y ? x+y : x-y' in c++ )
-    ( echo 1,2; echo 2,1 ) | csv-to-bin 2d | csv-eval --fields=x,y --binary=2d 'a=where(x<y,x+y,x-y)' | csv-from-bin 3d
+    # select operation based on condition
+    ( echo 1,2; echo 2,1 ) | csv-to-bin 2d | {script_name} --fields=x,y --binary=2d 'a=where(x<y,x+y,x-y)' | csv-from-bin 3d
+
+    # count number of occurances of "/" in a string
+    ( echo 'a'; echo 'a/b' ) | csv-to-bin s[36] | {script_name} --fields=path --binary=s[36] 'n=char.count(path,"/")' --append-binary=ui | csv-from-bin s[36],ui
+
+    # add and subtract a microsecond
+    ( echo 20150101T000000.000000; echo 20150101T000000.000010 ) | csv-to-bin t | {script_name} --fields=t --binary=t 'a=t+1;b=t-1' --append-binary=2t | csv-from-bin 3t
     
 """.format( script_name=sys.argv[0].split('/')[-1] )
   parser = argparse.ArgumentParser( description=description, epilog=epilog, formatter_class=lambda prog: argparse.RawTextHelpFormatter( prog, max_help_position=50 ) )  
