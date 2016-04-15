ae4f016e78dc3e9ee69534489c5580d73608aebf
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-04-15 11:37:39 +1000

    python/comma/csv/test/unit: increase indent to 4 spaces

--- python/comma/csv/test/unit/test_format.py
+++ python/comma/csv/test/unit/test_format.py
@@ -1,29 +1,31 @@
 import unittest
 import comma
 
-class test( unittest.TestCase ):
-  def test_expand( self ):
-    f = comma.csv.format.expand
-    self.assertEqual( f( '' ), '' )
-    self.assertEqual( f( 'ub' ), 'ub' )
-    self.assertEqual( f( 'ub,uw,ui,b,w,i,f,d,s[1],s[10],t' ), 'ub,uw,ui,b,w,i,f,d,s[1],s[10],t' )
-    self.assertEqual( f( '2ub,3s[1],4t' ), 'ub,ub,s[1],s[1],s[1],t,t,t,t' )
-    self.assertEqual( f( '2d,3d' ), 'd,d,d,d,d' )
-    self.assertEqual( f( '11d' ), 'd,d,d,d,d,d,d,d,d,d,d' )
-    self.assertEqual( f( 'invalid,2invalid' ), 'invalid,invalid,invalid' )
 
-  def test_time_format( self ):
-    self.assertEqual( comma.csv.format.NUMPY_TYPE_FROM_COMMA_TYPE['t'], 'datetime64[us]' )
+class test(unittest.TestCase):
+    def test_expand(self):
+        f = comma.csv.format.expand
+        self.assertEqual(f(''), '')
+        self.assertEqual(f('ub'), 'ub')
+        self.assertEqual(f('ub,uw,ui,b,w,i,f,d,s[1],s[10],t'), 'ub,uw,ui,b,w,i,f,d,s[1],s[10],t')
+        self.assertEqual(f('2ub,3s[1],4t'), 'ub,ub,s[1],s[1],s[1],t,t,t,t')
+        self.assertEqual(f('2d,3d'), 'd,d,d,d,d')
+        self.assertEqual(f('11d'), 'd,d,d,d,d,d,d,d,d,d,d')
+        self.assertEqual(f('invalid,2invalid'), 'invalid,invalid,invalid')
 
-  def test_to_numpy( self ):
-    f = comma.csv.format.to_numpy
-    self.assertRaises( comma.csv.format.format_error, f, '' )
-    self.assertTupleEqual( f( 'ub' ), ('u1',) )
-    self.assertTupleEqual( f( 'ub,uw,ui,b,w,i,f,d,s[1],s[10],t' ), ( 'u1', 'u2', 'u4', 'i1', 'i2', 'i4', 'f4', 'f8', 'S1', 'S10', 'datetime64[us]' ) )
-    self.assertTupleEqual( f( '2ub,3s[1],4t' ), ( 'u1', 'u1', 'S1', 'S1', 'S1', 'datetime64[us]', 'datetime64[us]', 'datetime64[us]', 'datetime64[us]' ) )
-    self.assertTupleEqual( f( '2d,3d' ), ( 'f8', 'f8', 'f8', 'f8', 'f8' ) )
-    self.assertTupleEqual( f( '11d' ), ( 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8' ) )
-    self.assertRaises( comma.csv.format.format_error, f, 'invalid,2invalid' )
+    def test_time_format(self):
+        self.assertEqual(comma.csv.format.NUMPY_TYPE_FROM_COMMA_TYPE['t'], 'datetime64[us]')
+
+    def test_to_numpy(self):
+        f = comma.csv.format.to_numpy
+        self.assertRaises(comma.csv.format.format_error, f, '')
+        self.assertTupleEqual(f('ub'), ('u1',))
+        self.assertTupleEqual(f('ub,uw,ui,b,w,i,f,d,s[1],s[10],t'), ('u1', 'u2', 'u4', 'i1', 'i2', 'i4', 'f4', 'f8', 'S1', 'S10', 'datetime64[us]'))
+        self.assertTupleEqual(f('2ub,3s[1],4t'), ('u1', 'u1', 'S1', 'S1', 'S1', 'datetime64[us]', 'datetime64[us]', 'datetime64[us]', 'datetime64[us]'))
+        self.assertTupleEqual(f('2d,3d'), ('f8', 'f8', 'f8', 'f8', 'f8'))
+        self.assertTupleEqual(f('11d'), ('f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8'))
+        self.assertRaises(comma.csv.format.format_error, f, 'invalid,2invalid')
+
 
 if __name__ == '__main__':
     unittest.main()
