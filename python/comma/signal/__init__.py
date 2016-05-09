42880d7c4665d4c51578ff45ef97e78c00b4aa7e
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-05-09 16:17:03 +1000

    python/signal: improve code formatting

--- python/comma/signal/__init__.py
+++ python/comma/signal/__init__.py
@@ -1,20 +1,20 @@
-from __future__ import absolute_import
 import signal
 import sys
 
-MANAGED_SIGNALS = tuple( [ signal.SIGINT, signal.SIGTERM, signal.SIGHUP ] )
+MANAGED_SIGNALS = tuple([signal.SIGINT, signal.SIGTERM, signal.SIGHUP])
 
-class is_shutdown:
-  def __init__( self ):
-    self.state = False
-    for s in MANAGED_SIGNALS:
-      signal.signal( s, self.switch_on )
 
-  def switch_on( self, signum, frame ):
-    print >> sys.stderr, "caught signal: ", signum
-    self.state = True
+class is_shutdown(object):
+    def __init__(self):
+        self.state = False
+        for s in MANAGED_SIGNALS:
+            signal.signal(s, self.switch_on)
 
-  def __nonzero__( self ):
-    return self.state
+    def switch_on(self, signum, frame):
+        print >> sys.stderr, "comma/python/signal: caught signal: ", signum
+        self.state = True
 
-signal.signal( signal.SIGPIPE, signal.SIG_DFL )
+    def __nonzero__(self):
+        return self.state
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
