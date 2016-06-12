7865968dd6b831ed3fb323e72941ebdf9023914c
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-06-12 14:41:59 +1000

    python/comma/cpp_bindings/CMakeLists.txt: expose parameters used in add_cpp_module

--- python/comma/cpp_bindings/csv.cpp
+++ python/comma/cpp_bindings/csv.cpp
@@ -1,7 +1,7 @@
 #include <boost/python.hpp>
 #include <comma/csv/format.h>
 
-BOOST_PYTHON_MODULE(csv)
+BOOST_PYTHON_MODULE( csv )
 {
     boost::python::class_< comma::csv::format >( "format", boost::python::init< std::string >() )
         .def( "size", &comma::csv::format::size );
