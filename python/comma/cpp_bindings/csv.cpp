2e41076cba0e3e317828de51989a94e3055e3e06
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-06-15 11:44:42 +1000

    python/comma/cpp_bindings/csv.cpp: tmp code just to test how values are passed

--- python/comma/cpp_bindings/csv.cpp
+++ python/comma/cpp_bindings/csv.cpp
@@ -1,9 +1,29 @@
 #include <boost/python.hpp>
 #include <comma/csv/format.h>
 
+struct blah
+{
+    blah() : value( 5 ) { std::cerr << "default constructor" << std::endl; }
+    blah( const blah& rhs ) : value( 6 ) { std::cerr << "copy constructor" << std::endl; }
+    const blah& operator=( const blah& rhs ) { std::cerr << "assignment" << std::endl; return *this; }
+    int value;
+};
+
+struct bound
+{
+    bound( const blah& b ) : value_( b.value ) {}
+    int value() const { return value_; }
+    int value_;
+};
+
 BOOST_PYTHON_MODULE( csv )
 {
     boost::python::class_< comma::csv::format >( "format", boost::python::init< const std::string& >() )
         .def( "size", &comma::csv::format::size );
+
+    boost::python::class_< blah >( "blah" );
+
+    boost::python::class_< bound >( "bound", boost::python::init< blah >() )
+        .def( "value", &bound::value );
 }
 
