f5e0ec3bb08c8a36d73d2d6505a933671539a795
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2016-06-29 12:30:11 +1000

    python/comma/io/readlines_unbuffered.py: skip blank lines by default

--- python/comma/io/readlines_unbuffered.py
+++ python/comma/io/readlines_unbuffered.py
@@ -1,10 +1,13 @@
 import sys
 
 
-def readlines_unbuffered(size, source=sys.stdin):
+def readlines_unbuffered(size, source=sys.stdin, skip_blank_lines=True):
     """
     read the given number of lines from source, such as stdin, without buffering
     note: builtin readlines() buffers the input and hence prevents flushing every line
+
+    blank lines are skipped unless skip_blank_lines is False
+    (a blank line is a line that has only whitespace characters or no characters)
     """
     if size >= 0:
         lines = ''
@@ -13,8 +16,13 @@ def readlines_unbuffered(size, source=sys.stdin):
             line = source.readline()
             if not line:
                 break
+            if skip_blank_lines and not line.strip():
+                continue
             lines += line
             number_of_lines += 1
     else:
-        lines = source.read()
+        if skip_blank_lines:
+            lines = '\n'.join(filter(lambda line: line.strip(), source.read().strip().splitlines()))
+        else:
+            lines = source.read()
     return lines
