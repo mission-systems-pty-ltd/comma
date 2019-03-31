5d20df9fb190f2f8425758f6ceeed87201626e87
vlaskine <vsevolod.vlaskine@gmail.com>
2019-03-31 13:17:13 +1100

    updated

--- name_value/applications/name-value-to-csv.cpp
+++ name_value/applications/name-value-to-csv.cpp
@@ -1,6 +1,5 @@
 // This file is part of comma, a generic and flexible library
 // Copyright (c) 2011 The University of Sydney
-// Copyright (c) 2019 Vsevolod Vlaskine
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
