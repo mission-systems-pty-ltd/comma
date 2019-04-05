a26109369fa48b6a59442b09d2a2c2f25790ad3b
vlaskine <vsevolod.vlaskine@gmail.com>
2019-04-05 21:51:11 +1100

    packed: implementing generic big/little endian values to introduce 3-byte big-endian...

--- packed/detail/endian.h
+++ packed/detail/endian.h
@@ -70,112 +70,6 @@ namespace comma { namespace packed { namespace detail {
 BOOST_STATIC_ASSERT( sizeof( float ) == 4 );
 BOOST_STATIC_ASSERT( sizeof( double ) == 8 );
 
-template < typename T > struct net_traits {};
-
-template <> struct net_traits< comma::uint16 >
-{
-    typedef comma::uint16 uint_of_same_size;
-    static comma::uint16 htobe( comma::uint16 v ) { return htobe16( v ); }
-    static comma::uint16 betoh( comma::uint16 v ) { return be16toh( v ); }
-    static comma::uint16 htole( comma::uint16 v ) { return htole16( v ); }
-    static comma::uint16 letoh( comma::uint16 v ) { return le16toh( v ); }
-};
-
-template <> struct net_traits< comma::int16 >
-{
-    typedef comma::uint16 uint_of_same_size;
-    static comma::int16 htobe( comma::int16 v ) { return htobe16( v ); }
-    static comma::int16 betoh( comma::int16 v ) { return be16toh( v ); }
-    static comma::int16 htole( comma::int16 v ) { return htole16( v ); }
-    static comma::int16 letoh( comma::int16 v ) { return le16toh( v ); }
-};
-
-template <> struct net_traits< comma::uint32 >
-{
-    typedef comma::uint32 uint_of_same_size;
-    static comma::uint32 htobe( comma::uint32 v ) { return htobe32( v ); }
-    static comma::uint32 betoh( comma::uint32 v ) { return be32toh( v ); }
-    static comma::uint32 htole( comma::uint32 v ) { return htole32( v ); }
-    static comma::uint32 letoh( comma::uint32 v ) { return le32toh( v ); }
-};
-
-template <> struct net_traits< comma::int32 >
-{
-    typedef comma::uint32 uint_of_same_size;
-    static comma::int32 htobe( comma::int32 v ) { return htobe32( v ); }
-    static comma::int32 betoh( comma::int32 v ) { return be32toh( v ); }
-    static comma::int32 htole( comma::int32 v ) { return htole32( v ); }
-    static comma::int32 letoh( comma::int32 v ) { return le32toh( v ); }
-};
-
-template <> struct net_traits< comma::uint64 >
-{
-    typedef comma::uint64 uint_of_same_size;
-    static comma::uint64 htobe( comma::uint64 v ) { return htobe64( v ); }
-    static comma::uint64 betoh( comma::uint64 v ) { return be64toh( v ); }
-    static comma::uint64 htole( comma::uint64 v ) { return htole64( v ); }
-    static comma::uint64 letoh( comma::uint64 v ) { return le64toh( v ); }
-};
-
-template <> struct net_traits< comma::int64 >
-{
-    typedef comma::uint64 uint_of_same_size;
-    static comma::int64 htobe( comma::int64 v ) { return htobe64( v ); }
-    static comma::int64 betoh( comma::int64 v ) { return be64toh( v ); }
-    static comma::int64 htole( comma::int64 v ) { return htole64( v ); }
-    static comma::int64 letoh( comma::int64 v ) { return le64toh( v ); }
-};
-
-template <> struct net_traits< float >
-{
-    typedef comma::uint32 uint_of_same_size;
-    static float htobe( float value )
-    {
-        uint_of_same_size v = net_traits< uint_of_same_size >::htobe( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< float* >( &v ) );
-    }
-    static float betoh( float value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::betoh( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< float* >( &v ) );
-    }
-    static float htole( float value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::htole( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< float* >( &v ) );
-    }
-    static float letoh( float value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::letoh( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< float* >( &v ) );
-    }
-};
-
-template <> struct net_traits< double >
-{
-    typedef comma::uint64 uint_of_same_size;
-    static double htobe( double value )
-    {
-        uint_of_same_size v = net_traits< uint_of_same_size >::htobe( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< double* >( &v ) );
-    }
-    static double betoh( double value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::betoh( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< double* >( &v ) );
-    }
-    static double htole( double value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::htole( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< double* >( &v ) );
-    }
-    static double letoh( double value )
-    { 
-        uint_of_same_size v = net_traits< uint_of_same_size >::letoh( *( reinterpret_cast< uint_of_same_size* >( &value ) ) );
-        return *( reinterpret_cast< double* >( &v ) );
-    }
-};
-
 enum endiannes { little = 0, big = 1 };
     
 template < endiannes Endianness, unsigned int Size, bool Signed, bool Floating = false > struct endian_traits { typedef typename comma::integer< Size, Signed >::type type; typedef typename comma::integer< Size, false >::type uint_of_same_size; };
@@ -183,6 +77,32 @@ template < endiannes Endianness > struct endian_traits< Endianness, 3, true > { 
 template < endiannes Endianness > struct endian_traits< Endianness, 3, false > { typedef comma::uint32 type; typedef comma::uint32 uint_of_same_size; };
 template < endiannes Endianness > struct endian_traits< Endianness, 4, true, true > { typedef float type; typedef comma::uint32 uint_of_same_size; };
 template < endiannes Endianness > struct endian_traits< Endianness, 8, true, true > { typedef double type; typedef comma::uint64 uint_of_same_size; };
+
+template < typename T > struct net_traits {};
+
+template <> struct net_traits< comma::uint16 >
+{
+    static comma::uint16 htobe( comma::uint16 v ) { return htobe16( v ); }
+    static comma::uint16 betoh( comma::uint16 v ) { return be16toh( v ); }
+    static comma::uint16 htole( comma::uint16 v ) { return htole16( v ); }
+    static comma::uint16 letoh( comma::uint16 v ) { return le16toh( v ); }
+};
+
+template <> struct net_traits< comma::uint32 >
+{
+    static comma::uint32 htobe( comma::uint32 v ) { return htobe32( v ); }
+    static comma::uint32 betoh( comma::uint32 v ) { return be32toh( v ); }
+    static comma::uint32 htole( comma::uint32 v ) { return htole32( v ); }
+    static comma::uint32 letoh( comma::uint32 v ) { return le32toh( v ); }
+};
+
+template <> struct net_traits< comma::uint64 >
+{
+    static comma::uint64 htobe( comma::uint64 v ) { return htobe64( v ); }
+    static comma::uint64 betoh( comma::uint64 v ) { return be64toh( v ); }
+    static comma::uint64 htole( comma::uint64 v ) { return htole64( v ); }
+    static comma::uint64 letoh( comma::uint64 v ) { return le64toh( v ); }
+};
 
 template < endiannes Endianness, unsigned int Size, bool Signed, bool Floating = false >
 struct endian : public packed::field< endian< Endianness, Size, Signed, Floating >, typename endian_traits< Endianness, Size, Signed, Floating >::type, Size >
@@ -201,6 +121,7 @@ struct endian : public packed::field< endian< Endianness, Size, Signed, Floating
     
     static void pack( char* storage, type value )
     {
+        uint_of_same_size* p = reinterpret_cast< uint_of_same_size* >( &value );
         if( Endianness == packed::detail::little ) // no point for further generics; should be optimized by compiler anyway
         {
             uint_of_same_size* p = reinterpret_cast< uint_of_same_size* >( &value );
@@ -208,16 +129,16 @@ struct endian : public packed::field< endian< Endianness, Size, Signed, Floating
         }
         else
         {
-            type v( net_traits< type >::htobe( value ) );
-            ::memcpy( storage, ( void* )&v, size );
+            uint_of_same_size i = net_traits< uint_of_same_size >::htobe( *p );
+            ::memcpy( storage, ( void* )&i, size );
         }
     }
 
     static type unpack( const char* storage ) // for floats it is a real hack, since there is no standard
     {
+        uint_of_same_size v = 0;
         if( Endianness == packed::detail::little ) // no point for further generics; should be optimized by compiler anyway
         {
-            uint_of_same_size v = 0;
             unsigned int shift = 0;
             unsigned int i = 0;
             for( ; i < size; ++i, shift += 8 )
@@ -233,9 +154,9 @@ struct endian : public packed::field< endian< Endianness, Size, Signed, Floating
         }
         else
         {
-            type value;
-            ::memcpy( ( void* )&value, storage, size );
-            return net_traits< type >::betoh( value );
+            ::memcpy( ( void* )&v, storage, size );
+            v = net_traits< uint_of_same_size >::betoh( v );
+            return *( reinterpret_cast< type* >( &v ) );
         }
     }
 
