2753b880b6e6c4a2ce0bec1ee3ae628824638d12
vlaskine <vsevolod.vlaskine@gmail.com>
2019-04-26 12:41:01 +1000

    csv-time-join: minor refactoring

--- csv/applications/csv-time-join.cpp
+++ csv/applications/csv-time-join.cpp
@@ -151,15 +151,8 @@ namespace comma { namespace visiting {
 
 template <> struct traits< Point >
 {
-    template < typename K, typename V > static void visit( const K&, const Point& p, V& v )
-    { 
-        v.apply( "t", p.timestamp );
-    }
-    
-    template < typename K, typename V > static void visit( const K&, Point& p, V& v )
-    {
-        v.apply( "t", p.timestamp );
-    }
+    template < typename K, typename V > static void visit( const K&, const Point& p, V& v ) { v.apply( "t", p.timestamp ); }
+    template < typename K, typename V > static void visit( const K&, Point& p, V& v ) { v.apply( "t", p.timestamp ); }
 };
     
 } } // namespace comma { namespace visiting {
@@ -168,17 +161,12 @@ enum class how { by_lower, by_upper, nearest, realtime };
 how method = how::by_lower;
 bool timestamp_only;
 bool select_only;
-
 comma::csv::options stdin_csv;
 comma::csv::options bounding_csv;
 boost::optional< boost::posix_time::time_duration > bound;
-
 typedef std::pair< boost::posix_time::ptime, std::string > timestring_t;
 
-boost::posix_time::ptime get_time( const Point& p )
-{
-    return p.timestamp ? *p.timestamp : boost::posix_time::microsec_clock::universal_time();
-}
+boost::posix_time::ptime get_time( const Point& p ) { return p.timestamp ? *p.timestamp : boost::posix_time::microsec_clock::universal_time(); }
 
 static void output_bounding( std::ostream& os, const timestring_t& bounding, bool stdin_first )
 {
@@ -209,16 +197,13 @@ static void output_bounding( std::ostream& os, const timestring_t& bounding, boo
 
 static void output_input( std::ostream& os, const timestring_t& input )
 {
-    if( stdin_csv.binary() ) { os.write( &input.second[0], stdin_csv.format().size() ); }
-    else { os << input.second; }
+    if( stdin_csv.binary() ) { os.write( &input.second[0], stdin_csv.format().size() ); } else { os << input.second; }
 }
 
 static void output( const timestring_t& input, const timestring_t& bounding, bool stdin_first )
 {
     if( bounding.first.is_infinity() ) { return; }
-
     if( bound && ( input.first - bounding.first > bound || bounding.first - input.first > bound )) { return; }
-
     if( stdin_first )
     {
         output_input( std::cout, input );
@@ -229,7 +214,6 @@ static void output( const timestring_t& input, const timestring_t& bounding, boo
         output_bounding( std::cout, bounding, stdin_first );
         output_input( std::cout, input );
     }
-
     if( !stdin_csv.binary() ) { std::cout << '\n'; }
     std::cout.flush();
 }
@@ -240,7 +224,6 @@ int main( int ac, char** av )
     {
         comma::signal_flag is_shutdown(comma::signal_flag::hard);
         comma::command_line_options options( ac, av, usage );
-
         if( options.exists( "--bash-completion" )) bash_completion( ac, av );
         options.assert_mutually_exclusive( "--by-lower,--by-upper,--nearest,--realtime" );
         if( options.exists( "--by-upper" )) { method = how::by_upper; }
@@ -253,7 +236,6 @@ int main( int ac, char** av )
         boost::optional< unsigned int > buffer_size = options.optional< unsigned int >( "--buffer" );
         if( options.exists( "--bound" ) ) { bound = boost::posix_time::microseconds( static_cast<unsigned int>(options.value< double >( "--bound" ) * 1000000 )); }
         stdin_csv = comma::csv::options( options, "t" );
-
         std::vector< std::string > unnamed = options.unnamed(
             "--by-lower,--by-upper,--nearest,--realtime,--select,--do-not-append,--timestamp-only,--time-only,--discard-bounding",
             "--binary,-b,--delimiter,-d,--fields,-f,--bound,--buffer,--verbose,-v" );
@@ -285,8 +267,7 @@ int main( int ac, char** av )
         if( stdin_csv.binary() ) { _setmode( _fileno( stdout ), _O_BINARY ); }
         #endif // #ifdef WIN32
 
-        comma::io::istream bounding_istream( comma::split( properties, ';' )[0]
-                                           , bounding_csv.binary() ? comma::io::mode::binary : comma::io::mode::ascii );
+        comma::io::istream bounding_istream( comma::split( properties, ';' )[0], bounding_csv.binary() ? comma::io::mode::binary : comma::io::mode::ascii );
         comma::csv::input_stream< Point > bounding_stream( *bounding_istream, bounding_csv );
 
         #ifndef WIN32
@@ -298,23 +279,18 @@ int main( int ac, char** av )
         #endif // #ifndef WIN32
 
         const Point* p = NULL;
-
         if( method == how::realtime )
         {
-            #ifndef WIN32
+            #ifdef WIN32
+            COMMA_THROW( comma::exception, "--realtime mode not supported in WIN32" );
+            #else
             bool end_of_input = false;
             bool end_of_bounds = false;
-            
-            boost::optional<timestring_t> joined_line;
-            
-            while (!is_shutdown && !end_of_input)
+            boost::optional< timestring_t > joined_line;
+            while( !is_shutdown && !end_of_input )
             {
-                if ( !bounding_stream.ready() && !stdin_stream.ready() )
-                {
-                    select.wait(boost::posix_time::milliseconds(1));
-                }
-                
-                if ( !is_shutdown && !end_of_input && ( stdin_stream.ready() || ( select.check() && select.read().ready( comma::io::stdin_fd ) ) ) )
+                if( !bounding_stream.ready() && !stdin_stream.ready() ) { select.wait(boost::posix_time::milliseconds(1)); }
+                if( !is_shutdown && !end_of_input && ( stdin_stream.ready() || ( select.check() && select.read().ready( comma::io::stdin_fd ) ) ) )
                 {
                     p = stdin_stream.read();
                     if( p )
@@ -328,9 +304,7 @@ int main( int ac, char** av )
                         end_of_input = true;
                     }
                 }
-                
-                if ( !is_shutdown && !end_of_bounds &&
-                   ( bounding_stream.ready() || ( select.check() && select.read().ready( bounding_istream.fd() ))))
+                if( !is_shutdown && !end_of_bounds && ( bounding_stream.ready() || ( select.check() && select.read().ready( bounding_istream.fd() ) ) ) )
                 {
                     p = bounding_stream.read();
                     if( p )
@@ -344,22 +318,17 @@ int main( int ac, char** av )
                     }
                 }
             }
-            if (is_shutdown) { comma::verbose << "got a signal" << std::endl; return 0; }
-            #else
-            COMMA_THROW(comma::exception, "--realtime mode not supported in WIN32");
-            #endif
+            if( is_shutdown ) { comma::verbose << "got a signal" << std::endl; return 0; }
+            #endif // #ifdef WIN32
         }
         else
         {
-            std::deque<timestring_t> bounding_queue;
+            std::deque< timestring_t > bounding_queue;
             bool next = true;
             bool bounding_data_available;
             bool upper_bound_added = false;
-
-            // add a fake entry for an lower bound to allow stdin before first bound to match
-            bounding_queue.push_back( std::make_pair( boost::posix_time::neg_infin, "" ));
-
-            while( ( stdin_stream.ready() || ( std::cin.good() && !std::cin.eof() ) ) )
+            bounding_queue.push_back( std::make_pair( boost::posix_time::neg_infin, "" ) ); // add a fake entry for an lower bound to allow stdin before first bound to match
+            while( stdin_stream.ready() || ( std::cin.good() && !std::cin.eof() ) )
             {
                 if( !std::cin.good() ) { select.read().remove( 0 ); }
                 if( !bounding_istream->good() ) { select.read().remove( bounding_istream.fd() ); }
@@ -371,52 +340,35 @@ int main( int ac, char** av )
                 //check so we do not block
                 bool bounding_stream_ready = bounding_stream.ready();
                 bool stdin_stream_ready = stdin_stream.ready();
-
                 if( next )
                 {
                     if( !bounding_stream_ready || !stdin_stream_ready )
                     {
-                        if( !bounding_stream_ready && !stdin_stream_ready )
-                        {
-                            select.wait( boost::posix_time::milliseconds(10) );
-                        }
-                        else
-                        {
-                            select.check();
-                        }
+                        if( !bounding_stream_ready && !stdin_stream_ready ) { select.wait( boost::posix_time::milliseconds( 10 ) ); }
+                        else { select.check(); }
                         if( select.read().ready( bounding_istream.fd() )) { bounding_stream_ready = true; }
-                        if( select.read().ready(0) ) { stdin_stream_ready=true; }
+                        if( select.read().ready(0) ) { stdin_stream_ready = true; }
                     }
                 }
                 else
                 {
                     if( !bounding_stream_ready )
                     {
-                        bounding_stream_select.wait( boost::posix_time::milliseconds(10) );
+                        bounding_stream_select.wait( boost::posix_time::milliseconds( 10 ) );
                         if( bounding_stream_select.read().ready( bounding_istream.fd() )) { bounding_stream_ready=true; }
                     }
                 }
                 #endif //#ifdef WIN32
-
                 //keep storing available bounding data
                 if( bounding_stream_ready )
                 {
                     if( !buffer_size || bounding_queue.size() < *buffer_size || discard_bounding )
                     {
                         const Point* q = bounding_stream.read();
-                        if( q )
-                        {
-                            bounding_queue.push_back( std::make_pair( get_time( *q ), bounding_stream.last() ));
-                        }
-                        else
-                        {
-                            bounding_data_available=false;
-                        }
+                        if( q ) { bounding_queue.push_back( std::make_pair( get_time( *q ), bounding_stream.last() )); }
+                        else { bounding_data_available = false; }
                     }
-                    if( buffer_size && bounding_queue.size() > *buffer_size && discard_bounding )
-                    {
-                        bounding_queue.pop_front();
-                    }
+                    if( buffer_size && bounding_queue.size() > *buffer_size && discard_bounding ) { bounding_queue.pop_front(); }
                 }
                 if( !upper_bound_added && bounding_istream->eof() )
                 {
@@ -424,53 +376,41 @@ int main( int ac, char** av )
                     bounding_queue.push_back( std::make_pair( boost::posix_time::pos_infin, "" ));
                     upper_bound_added = true;
                 }
-
                 //if we are done with the last bounded point get next
                 if( next )
                 {
-                    if(!stdin_stream_ready) { continue; }
+                    if( !stdin_stream_ready ) { continue; }
                     p = stdin_stream.read();
                     if( !p ) { break; }
                 }
-
-                boost::posix_time::ptime t = get_time(*p);
-              
+                boost::posix_time::ptime t = get_time( *p );
                 //get bound
-                while(bounding_queue.size()>=2)
-                {
-                    if( t < bounding_queue[1].first ) { break; }
-                    bounding_queue.pop_front();
-                }
-
-                if(bounding_queue.size()<2)
+                for( ; bounding_queue.size() >= 2 && t >= bounding_queue[1].first; bounding_queue.pop_front() );
+                if( bounding_queue.size() < 2 )
                 {
                     //bound not found
                     //do we have more data?
-                    if(!bounding_data_available) { break; }
-                    next=false;
+                    if( !bounding_data_available ) { break; }
+                    next = false;
                     continue;
                 }
-
                 //bound available
-
                 if( method == how::by_lower && t < bounding_queue.front().first )
                 {
                     next = true;
                     continue;
                 }
-
                 bool is_first = ( method == how::by_lower )
                     || ( method == how::nearest && ( t - bounding_queue[0].first ) < ( bounding_queue[1].first - t ));
-
                 const timestring_t& chosen_bound = is_first ? bounding_queue[0] : bounding_queue[1];;
                 timestring_t input_line = std::make_pair( t, stdin_stream.last() );
-
                 output( input_line, chosen_bound, stdin_first );
-                next=true;
+                next = true;
             }
         }
         return 0;     
     }
     catch( std::exception& ex ) { std::cerr << "csv-time-join: " << ex.what() << std::endl; }
     catch( ... ) { std::cerr << "csv-time-join: unknown exception" << std::endl; }
+    return 1;
 }
