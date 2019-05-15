9bf0271887d6eecb900d2ed20e23edcee07f1530
dave <dave@acfr.usyd.edu.au>
2019-05-15 11:33:49 +1000

    csv-shape: added flatten operation

--- csv/applications/csv-shape.cpp
+++ csv/applications/csv-shape.cpp
@@ -49,6 +49,7 @@ static void usage( bool verbose=false )
     std::cerr << "    concatenate: group input records for concatenation into output records." << std::endl;
     std::cerr << "                 The user can choose non-overlapping or overlapping grouping" << std::endl;
     std::cerr << "                 (sliding window) mode." << std::endl;
+    std::cerr << "    flatten:     flatten block-wise data so it's one record per block" << std::endl;
     std::cerr << "    loop:        same as concatenate, but additionally the input record is" << std::endl;
     std::cerr << "                 concatenated with the first record (hence, 'loop')." << std::endl;
     std::cerr << "                 Always uses the sliding window for overlapping groups" << std::endl;
@@ -75,6 +76,10 @@ static void usage( bool verbose=false )
     std::cerr << "      --bidirectional; output records in both directions (e.g. a,b; b,a)" << std::endl;
     std::cerr << "      --reverse; output records in reverse order (e.g. b,a)" << std::endl;
     std::cerr << std::endl;
+    std::cerr << "   flatten options" << std::endl;
+    std::cerr << "      --header,--header-fields=[<fields>]: header fields to output" << std::endl;
+    std::cerr << "      --output,--output-fields,-o=<fields>: recurring fields to output" << std::endl;
+    std::cerr << std::endl;
     if( verbose )
     {
         std::cerr << "examples" << std::endl;
@@ -92,6 +97,10 @@ static void usage( bool verbose=false )
         std::cerr << "         2 to 6 create the second record, and so forth." << std::endl;
         std::cerr << "            seq 1 10 | csv-shape concatenate -n 5 --sliding-window" << std::endl;
         std::cerr << std::endl;
+        std::cerr << "   flatten" << std::endl;
+        std::cerr << "      echo -e \"a,0,1,2\\nb,0,3,4\\nc,1,5,6\\nd,1,7,8\" \\" << std::endl;
+        std::cerr << "          | csv-shape flatten --fields s,block,a,b --output a,b --header s,block" << std::endl;
+        std::cerr << std::endl;
         std::cerr << "csv options" << std::endl;
         std::cerr << comma::csv::options::usage() << std::endl;
     }
@@ -103,20 +112,7 @@ static void usage( bool verbose=false )
     exit( 0 );
 }
 
-// There is nothing to do in this case - binary data
-static void simple_binary_pass_through(const comma::csv::format& f, bool flush=false)
-{
-    std::vector< char > buffer( f.size(), '\0' );
-    while( std::cin.good() && !std::cin.eof() )
-    {
-        if( std::cin.read( &buffer[0], buffer.size() ) ) {
-            std::cout.write( &buffer[0], buffer.size() );
-            if( flush ) { std::cout.flush(); }
-        }
-    }
-}
-
-bool is_binary;
+std::string operation;
 
 class concatenate_
 {
@@ -136,13 +132,12 @@ public:
 
     int run( const comma::command_line_options& options, const comma::csv::options& csv)
     {
-        std::vector< std::string > unnamed = options.unnamed( "--size,-n,--sliding-window,-w,--step,--verbose,-v", "-.*" );
-        std::string operation = unnamed[0];
+        is_binary_ = csv.binary();
         looping_ = ( operation == "loop");
         use_sliding_window_ = ( looping_ || options.exists("--sliding-window,-w") );
         reverse_ = options.exists("--reverse");
         bidirectional_ = options.exists("--bidirectional");
-        if( !use_sliding_window_ && is_binary ) { simple_binary_pass_through(csv.format(), csv.flush); return 0; };
+        if( !use_sliding_window_ && is_binary_ ) { simple_binary_pass_through(csv.format(), csv.flush); return 0; };
         size_ = looping_ ? options.value("--size,-n", 2) : options.value< comma::uint32 >("--size,-n");
         step_ = options.value( "--step",1 );
         if( size_ < 2 ) { std::cerr <<  comma::verbose.app_name() << ": expected --size,-n= value to be greater than 1" << std::endl; return 1; }
@@ -181,6 +176,7 @@ public:
     struct input_t { comma::uint32 block = 0; };
 
 private:
+    bool is_binary_;
     bool use_sliding_window_;
     bool bidirectional_;
     bool reverse_;
@@ -190,6 +186,19 @@ private:
     comma::uint32 block_;
     comma::uint32 step_;
     comma::uint32 expected_records_;
+
+    // There is nothing to do in this case - binary data
+    static void simple_binary_pass_through(const comma::csv::format& f, bool flush=false)
+    {
+        std::vector< char > buffer( f.size(), '\0' );
+        while( std::cin.good() && !std::cin.eof() )
+        {
+            if( std::cin.read( &buffer[0], buffer.size() ) ) {
+                std::cout.write( &buffer[0], buffer.size() );
+                if( flush ) { std::cout.flush(); }
+            }
+        }
+    }
 
     bool verify( const std::deque<std::string>& deque )
     {
@@ -213,15 +222,15 @@ private:
         if (!reverse_)
         {
             std::cout.write( &( deque.front()[ 0 ] ), deque.front().size() );
-            auto is = deque.cbegin(); while( ( is + 1 ) != deque.cend() ) { if(!is_binary){ std::cout << csv.delimiter; } is += step_; std::cout.write( &(*is)[0], is->size() ); }
-            if(!is_binary){ std::cout << std::endl; }
+            auto is = deque.cbegin(); while( ( is + 1 ) != deque.cend() ) { if(!is_binary_){ std::cout << csv.delimiter; } is += step_; std::cout.write( &(*is)[0], is->size() ); }
+            if(!is_binary_){ std::cout << std::endl; }
             if (csv.flush) { std::cout.flush(); }
         }
         if (bidirectional_ || reverse_ )
         {
             std::cout.write( &( deque.back()[0] ), deque.back().size() );
-            auto is = deque.crbegin(); while( ( is + 1 ) != deque.crend() ) { if(!is_binary){ std::cout << csv.delimiter; } is += step_; std::cout.write( &(*is)[0], is->size() ); }
-            if(!is_binary){ std::cout << std::endl; }
+            auto is = deque.crbegin(); while( ( is + 1 ) != deque.crend() ) { if(!is_binary_){ std::cout << csv.delimiter; } is += step_; std::cout.write( &(*is)[0], is->size() ); }
+            if(!is_binary_){ std::cout << std::endl; }
             if (csv.flush) { std::cout.flush(); }
         }
     }
@@ -236,6 +245,149 @@ private:
             first.pop_front();
         }
     }
+};
+
+class flatten_
+{
+public:
+    struct field
+    {
+        std::string name;
+        boost::optional< unsigned int > index;
+        unsigned int offset;
+        unsigned int size;
+        field( const std::string& name ) : name( name ) {}
+    };
+
+    flatten_( const comma::command_line_options& options, const comma::csv::options& csv )
+    {
+        for( auto f: comma::split( options.value< std::string >( "--output-fields,--output,-o" ), ',' ))
+        {
+            if( !f.empty() ) { output_fields.push_back( field( f )); }
+        }
+
+        for( auto f: comma::split( options.value< std::string >( "--header-fields,--header", "" ), ',' ))
+        {
+            if( !f.empty() ) { header_fields.push_back( field( f )); }
+        }
+
+        std::vector< std::string > input_fields = comma::split( csv.fields, "," );
+        for( unsigned int i = 0; i < input_fields.size(); ++i )
+        {
+            for( auto& f: output_fields )
+            {
+                if( f.name == input_fields[i] )
+                {
+                    f.index = i;
+                    if( csv.binary() )
+                    {
+                        f.offset = csv.format().offset( i ).offset;
+                        f.size = csv.format().offset( i ).size;
+                    }
+                }
+            }
+            for( auto& f: header_fields )
+            {
+                if( f.name == input_fields[i] )
+                {
+                    f.index = i;
+                    if( csv.binary() )
+                    {
+                        f.offset = csv.format().offset( i ).offset;
+                        f.size = csv.format().offset( i ).size;
+                    }
+                }
+            }
+            if( input_fields[i] == "block" )
+            {
+                block_field = field( input_fields[i] );
+                block_field->index = i;
+                if( csv.binary() )
+                {
+                    block_field->offset = csv.format().offset( i ).offset;
+                    block_field->size = csv.format().offset( i ).size;
+                }
+            }
+        }
+    }
+
+    int run( const comma::csv::options& csv )
+    {
+        if( !block_field ) { std::cerr << comma::verbose.app_name() << ": " << operation << " operation requires block field" << std::endl; return 1; }
+        if( output_fields.empty() ) { std::cerr << comma::verbose.app_name() << ": " << operation << " operation requires at least one output field" << std::endl; return 1; }
+        for( auto f: output_fields )
+        {
+            if( !f.index ) { std::cerr << comma::verbose.app_name() << ": \"" << f.name << "\" not found in input fields " << csv.fields << std::endl; return 1; }
+        }
+        for( auto f: header_fields )
+        {
+            if( !f.index ) { std::cerr << comma::verbose.app_name() << ": \"" << f.name << "\" not found in input fields " << csv.fields << std::endl; return 1; }
+        }
+
+        comma::uint32 current_block = std::numeric_limits< comma::uint32 >::max();
+        if( csv.binary() )
+        {
+            std::vector< char > buf( csv.format().size() );
+            comma::uint32* block;
+
+            while( std::cin.good() && !std::cin.eof() )
+            {
+                std::cin.read( &buf[0], csv.format().size() );
+                if( std::cin.gcount() == 0 ) { continue; }
+                if( std::cin.gcount() < int( csv.format().size() ))
+                {
+                    std::cerr << comma::verbose.app_name() << ": expected " << csv.format().size()
+                              << " bytes, got only " << std::cin.gcount() << std::endl;
+                    return 1;
+                }
+                block = reinterpret_cast< comma::uint32* >( &buf[ block_field->offset ] );
+                if( current_block != *block )
+                {
+                    for( auto f: header_fields ) { std::cout.write( &buf[ f.offset ], f.size ); }
+                    current_block = *block;
+                }
+                for( auto f: output_fields ) { std::cout.write( &buf[ f.offset ], f.size ); }
+            }
+        }
+        else
+        {
+            std::string delimiter;
+            bool first_line = true;
+            while( std::cin.good() && !std::cin.eof() )
+            {
+                std::string line;
+                std::getline( std::cin, line );
+                if( !line.empty() && *line.rbegin() == '\r' ) { line = line.substr( 0, line.length() - 1 ); } // windows... sigh...
+                if( line.empty() ) { continue; }
+                std::vector< std::string > v = comma::split( line, csv.delimiter );
+                comma::uint32 block = boost::lexical_cast< comma::uint32 >( v[ *(block_field->index) ] );
+                if( current_block != block )
+                {
+                    if( !first_line ) { std::cout << std::endl; }
+                    delimiter = "";
+                    for( auto f: header_fields )
+                    {
+                        std::cout << delimiter << v[ *(f.index) ];
+                        delimiter = csv.delimiter;
+                    }
+                    current_block = block;
+                    first_line = false;
+                }
+                for( auto f: output_fields )
+                {
+                    std::cout << delimiter << v[ *(f.index) ];
+                    delimiter = csv.delimiter;
+                }
+            }
+            std::cout << std::endl;
+        }
+        return 0;
+    }
+
+private:
+    std::vector< field > output_fields;
+    std::vector< field > header_fields;
+    boost::optional< field > block_field;
 };
 
 namespace comma { namespace visiting {
@@ -284,11 +436,11 @@ int main( int ac, char** av )
         std::vector< std::string > unnamed = options.unnamed( "--size,-n,--sliding-window,-w,--step,--verbose,-v", "-.*" );
         comma::csv::options csv( options );
         csv.full_xpath = false;
-        if (csv.fields.empty()) { csv.fields="a"; }
-        is_binary = csv.binary();
-        if( unnamed.empty() ) { std::cerr << comma::verbose.app_name() << ": please specify operations" << std::endl; exit( 1 ); }
-        std::string operation = unnamed[0];
-        if( operation == "concatenate" || operation == "loop" ) { return concatenate_().run(options, csv); }
+        if( csv.fields.empty() ) { csv.fields = "a"; }
+        if( unnamed.empty() ) { std::cerr << comma::verbose.app_name() << ": please specify operation" << std::endl; exit( 1 ); }
+        operation = unnamed[0];
+        if( operation == "concatenate" || operation == "loop" ) { return concatenate_().run( options, csv ); }
+        if( operation == "flatten" ) { return flatten_( options, csv ).run( csv ); }
         if( operation == "repeat" ) { return repeat_( options, csv ); }
         std::cerr << comma::verbose.app_name() << ": operation not supported or unknown: '" << operation << '\'' << std::endl;
         return 1;
