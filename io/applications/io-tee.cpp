d4c8b1304d3e3d7b2d0f556cd6bcf5ee0b71df24
vlaskine <vsevolod.vlaskine@gmail.com>
2019-05-31 13:48:05 +1000

    io-tee: --dry-run made exiting before checking whether output file is writable; output filename quoted in command to support filenames with special characters

--- io/applications/io-tee.cpp
+++ io/applications/io-tee.cpp
@@ -41,11 +41,9 @@
 #include "../../io/select.h"
 #include "../../io/stream.h"
 
-static const char *app_name = "io-tee";
-
 static void show_usage()
 {
-    std::cerr << "Usage: " << app_name << " <output file> [options ... --] <command ...>\n";
+    std::cerr << "Usage: io-tee <output file> [options ... --] <command ...>\n";
 }
 
 static void show_help( bool verbose = false )
@@ -63,14 +61,14 @@ static void show_help( bool verbose = false )
         << "    --verbose,-v: more output" << std::endl
         << std::endl
         << "Note that only single commands are supported; to run multiple commands (or a pipeline), put them inside a bash function:" << std::endl
-        << "*** IMPORTANT *** use \"export -f function_name\" to make the function visible to " << app_name << "." << std::endl
-        << "Remember that " << app_name << " will not have access to the unexported variables, so pass any required values as function arguments." << std::endl
+        << "*** IMPORTANT *** use \"export -f function_name\" to make the function visible to io-tee." << std::endl
+        << "Remember that io-tee will not have access to the unexported variables, so pass any required values as function arguments." << std::endl
         << "On Ubuntu 16.04, io-tee might fail to discover a bash function even if it is exported." << std::endl
         << "In this case, comma_tee_function defined in comma-application-util should be used." << std::endl
         << std::endl
         << "If any options are used (such as --unbuffered), \"--\" must precede the command." << std::endl
         << std::endl
-        << "A note about using \"grep\": be aware grep returns 1 if the pattern is not found, which will make " << app_name << " think the command failed." << std::endl
+        << "A note about using \"grep\": be aware grep returns 1 if the pattern is not found, which will make io-tee think the command failed." << std::endl
         << "To avoid this, call grep inside a function like this: grep (pattern) || true." << std::endl
         << std::endl
         << "Example 1:" << std::endl
@@ -127,19 +125,19 @@ int main( int ac, char **av )
             else if ( av[n] == std::string( "--help" ) || av[n] == std::string( "-h" ) ) { show_help(); exit( 0 ); }
         }
         int command_offset = ( dashdash_pos == -1 ? 2 : dashdash_pos + 1 );
-        if ( command_offset >= ac ) { std::cerr << app_name << ": missing command; "; show_usage(); exit( 1 ); }
+        if ( command_offset >= ac ) { std::cerr << "io-tee: missing command; "; show_usage(); exit( 1 ); }
         // if there is no "--", there can be no command line options, just the output filename
         int options_ac = ( dashdash_pos == -1 ? 2 : dashdash_pos );
         if ( debug )
         {
-            std::cerr << app_name << ": options_ac=" << options_ac << "; command line: " << app_name;
+            std::cerr << "io-tee: options_ac=" << options_ac << "; command line: io-tee";
             for ( int m = 1; m < ac; ++m ) { std::cerr << ' ' << av[m]; }
             std::cerr << std::endl;
         }
         comma::command_line_options options( options_ac, av );
         const std::vector< std::string >& unnamed = options.unnamed( "--unbuffered,-u,--verbose,-v,--debug,--dry-run,--dry,--append,-a", "-.*" );
-        if( unnamed.empty() ) { std::cerr << app_name << ": please specify output file name" << std::endl; return 1; }
-        if( unnamed.size() > 1 ) { std::cerr << app_name << ": expected one output filename, got: " << comma::join( unnamed, ' ' ) << std::endl; return 1; }
+        if( unnamed.empty() ) { std::cerr << "io-tee: please specify output file name" << std::endl; return 1; }
+        if( unnamed.size() > 1 ) { std::cerr << "io-tee: expected one output filename, got: " << comma::join( unnamed, ' ' ) << std::endl; return 1; }
         std::string outfile = unnamed[0];
         // bash -c only takes a single argument, so put the whole command in single quotes, then double quote each individual argument
         std::string command = "bash -c '" + escape_quotes( av[command_offset] );
@@ -147,24 +145,24 @@ int main( int ac, char **av )
         bool append_to_outfile = options.exists( "--append,-a" );
         if( append_to_outfile ) { command += " >> "; }
         else { command += " > "; }
-        command += outfile;
+        command += '"' + outfile + '"';
         command += "'";
         bool unbuffered = options.exists( "--unbuffered,-u" );
         bool verbose = options.exists( "--verbose,-v" );
         if ( debug ) { verbose = true; }
-        if( !file_is_writable( outfile, append_to_outfile ) ) { std::cerr << app_name << ": cannot write to " << outfile << std::endl; exit( 1 ); }
         if( options.exists( "--dry-run,--dry" ) ) { std::cout << command << std::endl; return 0; }
-        if( verbose ) { std::cerr << app_name << ": will run command: " << command << std::endl; }
+        if( verbose ) { std::cerr << "io-tee: will run command: " << command << std::endl; }
+        if( !file_is_writable( outfile, append_to_outfile ) ) { std::cerr << "io-tee: cannot write to " << outfile << std::endl; exit( 1 ); }
         std::cout.flush();
         pipe = ::popen( &command[0], "w" );
-        if( pipe == NULL ) { std::cerr << app_name << ": failed to open pipe; command: " << command << std::endl; return 1; }
+        if( pipe == NULL ) { std::cerr << "io-tee: failed to open pipe; command: " << command << std::endl; return 1; }
         boost::array< char, 0xffff > buffer;
-        if ( debug ) { std::cerr << app_name << ": created buffer" << std::endl; }
+        if ( debug ) { std::cerr << "io-tee: created buffer" << std::endl; }
         comma::io::select stdin_select;
-        if ( debug ) { std::cerr << app_name << ": constructed comma::io::select" << std::endl; }
-        if( unbuffered ) { stdin_select.read().add( 0 ); if ( debug ) { std::cerr << app_name << ": did initial unbuffered read" << std::endl; } }
+        if ( debug ) { std::cerr << "io-tee: constructed comma::io::select" << std::endl; }
+        if( unbuffered ) { stdin_select.read().add( 0 ); if ( debug ) { std::cerr << "io-tee: did initial unbuffered read" << std::endl; } }
         comma::io::istream is( "-", comma::io::mode::binary );
-        if ( debug ) { std::cerr << app_name << ": opened input stream" << std::endl; }
+        if ( debug ) { std::cerr << "io-tee: opened input stream" << std::endl; }
         if( unbuffered )
         {
             std::ios_base::sync_with_stdio( false ); // unsync to make rdbuf()->in_avail() working
@@ -172,51 +170,51 @@ int main( int ac, char **av )
         }
         while( std::cin.good() )
         {
-            if ( debug ) { std::cerr << app_name << ": loop" << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: loop" << std::endl; }
             std::size_t bytes_to_read = buffer.size();
             if( unbuffered )
             {
-                if ( debug ) { std::cerr << app_name << ": calling stdin_select.wait(1)" << std::endl; }
+                if ( debug ) { std::cerr << "io-tee: calling stdin_select.wait(1)" << std::endl; }
                 if( stdin_select.wait( boost::posix_time::seconds( 1 ) ) == 0 ) { continue; }
-                if ( debug ) { std::cerr << app_name << ": after stdin_select.wait" << std::endl; }
+                if ( debug ) { std::cerr << "io-tee: after stdin_select.wait" << std::endl; }
                 std::size_t available = is.available_on_file_descriptor();
-                if ( debug ) { std::cerr << app_name << ": " << available << " bytes available" << std::endl; }
+                if ( debug ) { std::cerr << "io-tee: " << available << " bytes available" << std::endl; }
                 bytes_to_read = std::min( available, buffer.size() );
             }
-            if ( debug ) { std::cerr << app_name << ": bytes_to_read = " << bytes_to_read << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: bytes_to_read = " << bytes_to_read << std::endl; }
             std::cin.read( &buffer[0], bytes_to_read );
-            if ( debug ) { std::cerr << app_name << ": cin.gcount is " << std::cin.gcount() << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: cin.gcount is " << std::cin.gcount() << std::endl; }
             if( std::cin.gcount() <= 0 ) { break; }
             std::size_t gcount = std::cin.gcount();
-            if ( debug ) { std::cerr << app_name << ": writing " << gcount << " bytes to stdout" << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: writing " << gcount << " bytes to stdout" << std::endl; }
             std::cout.write( &buffer[0], gcount );
-            if ( debug ) { std::cerr << app_name << ": writing " << gcount << " bytes to pipe" << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: writing " << gcount << " bytes to pipe" << std::endl; }
             int r = ::fwrite( &buffer[0], sizeof( char ), gcount, pipe );
-            if ( debug ) { std::cerr << app_name << ": fwrite to pipe returned " << r << std::endl; }
+            if ( debug ) { std::cerr << "io-tee: fwrite to pipe returned " << r << std::endl; }
             if( r != (int) gcount )
             { 
-                std::cerr << app_name << ": error on pipe: " << std::strerror( errno ) <<  std::endl;
+                std::cerr << "io-tee: error on pipe: " << std::strerror( errno ) <<  std::endl;
                 ::pclose( pipe );
                 return 1;
             }
             if( unbuffered )
             { 
-                if ( debug ) { std::cerr << app_name << ": flushing stdout" << std::endl; }
+                if ( debug ) { std::cerr << "io-tee: flushing stdout" << std::endl; }
                 std::cout.flush();
-                if ( debug ) { std::cerr << app_name << ": flushing pipe" << std::endl; }
-                if ( ::fflush( pipe ) != 0 ) { std::cerr << app_name << ": flushing pipe failed: " << std::strerror( errno ) << "; command was: " << command << std::endl; ::pclose( pipe ); exit( 1 ); }
-                if ( debug ) { std::cerr << app_name << ": flushed stdout and pipe " << std::endl; }
+                if ( debug ) { std::cerr << "io-tee: flushing pipe" << std::endl; }
+                if ( ::fflush( pipe ) != 0 ) { std::cerr << "io-tee: flushing pipe failed: " << std::strerror( errno ) << "; command was: " << command << std::endl; ::pclose( pipe ); exit( 1 ); }
+                if ( debug ) { std::cerr << "io-tee: flushed stdout and pipe " << std::endl; }
             }
         }
         std::cout.flush();
         ::fflush( pipe );
         int result = ::pclose( pipe );
-        if ( result == -1 ) { std::cerr << app_name << ": pipe error: " << std::strerror( errno ) << "; command was: " << command << std::endl; exit( 1 ); }
-        else if ( result != 0 ) { std::cerr << app_name << ": command failed: " << command << std::endl; return 1; }
+        if ( result == -1 ) { std::cerr << "io-tee: pipe error: " << std::strerror( errno ) << "; command was: " << command << std::endl; exit( 1 ); }
+        else if ( result != 0 ) { std::cerr << "io-tee: command failed: " << command << std::endl; return 1; }
         return 0;
     }
-    catch( std::exception& ex ) { std::cerr << app_name << ": " << ex.what() << std::endl; }
-    catch( ... ) { std::cerr << app_name << ": unknown exception" << std::endl; }
+    catch( std::exception& ex ) { std::cerr << "io-tee: " << ex.what() << std::endl; }
+    catch( ... ) { std::cerr << "io-tee: unknown exception" << std::endl; }
     if( pipe ) { ::pclose( pipe ); }
     return 1;
 }
