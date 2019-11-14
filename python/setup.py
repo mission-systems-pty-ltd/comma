cc89ca7d091685c534de5e3272edde6f3f67710d
seva <seva>
2019-11-14 14:18:12 +1100

    python/setup.py: fixed url

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ setup(
         name                = 'comma',
         version             = open('comma/version.py').readlines()[-1].strip().split()[-1].strip('\"'),
         description         = 'comma python utilties',
-        url                 = 'https://github.com/acfr/comma',
+        url                 = 'https://gitlab.com/orthographic/comma',
         license             = 'BSD 3-Clause',
         packages            = [ 'comma', 'comma.csv', 'comma.csv.applications', 'comma.io', 'comma.numpy', 'comma.signal', 'comma.util', 'comma.cpp_bindings', 'comma.application' ],
         package_dir         = { 'comma.cpp_bindings': 'comma/cpp_bindings' },
