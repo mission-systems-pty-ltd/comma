dd5882ed9ff4838041955d62f7c71f229b995176
vlaskine <vsevolod.vlaskine@gmail.com>
2019-11-18 20:57:24 +1100

    python/comma: porting to python3; stream: convert mapped stuff to the list, with potential performance deterioration; in progress...

--- python/comma/csv/stream.py
+++ python/comma/csv/stream.py
@@ -242,9 +242,9 @@ class stream(object):
     def _tie_binary(self, tied_array, array): return merge_arrays(tied_array, array)
 
     def _tie_ascii(self, tied_buffer, unrolled_array):
-        for tied_line, scalars in izip(tied_buffer, unrolled_array): yield self.delimiter.join([tied_line] + self._strings(scalars))
+        for tied_line, scalars in izip(tied_buffer, unrolled_array): yield self.delimiter.join([tied_line] + list(self._strings(scalars)))
 
-    def _toline(self, scalars): return self.delimiter.join(self._strings(scalars))
+    def _toline(self, scalars): return list(self.delimiter.join(self._strings(scalars)))
 
     def dump(self, mask=None):
         """
