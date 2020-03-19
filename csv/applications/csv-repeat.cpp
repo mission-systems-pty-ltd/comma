0defca981e09037ec4e883a03a35f07bcf7385cc
seva <seva>
2020-03-19 19:02:34 +1100

    csv-repeat: --timestamped: implemented; unit test: todo...

--- csv/applications/csv-repeat.cpp
+++ csv/applications/csv-repeat.cpp
@@ -29,6 +29,7 @@
 
 /// @author dave jennings
 
+#include <functional>
 #include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
@@ -73,6 +74,11 @@ void usage( bool verbose = false )
     std::cerr << "            warning: currently is very simplistic; see todo comments in the code to make it more robust" << std::endl;
     std::cerr << "    --period=[<seconds>]: period of repeated record" << std::endl;
     std::cerr << "    --timeout,-t=[<seconds>]: timeout before repeating the last record; if not specified, timeout is set to --period" << std::endl;
+    std::cerr << "    --timestamped: use input timestamp for repeating; currently, would do blocking read" << std::endl;
+    std::cerr << "                   convenient for filling holes in data in offline processing" << std::endl;
+    std::cerr << "      --timestamped options" << std::endl;
+    std::cerr << "          --from=[<time>]; if first timestamp greater than <time>, fill the hole with the first record" << std::endl;
+    std::cerr << "          --to=[<time>]; if last timestamp less than <time>, fill the hole with the last record" << std::endl;
     std::cerr << "    --verbose,-v: more output" << std::endl;
     std::cerr << std::endl;
     std::cerr << "    if --period is not set, --timeout acts as a watchdog. If no input is seen" << std::endl;
@@ -107,6 +113,11 @@ void usage( bool verbose = false )
     exit( 0 );
 }
 
+struct input_t
+{
+    boost::posix_time::ptime time;
+};
+
 struct output_t
 {
     boost::posix_time::ptime time;
@@ -116,6 +127,12 @@ struct output_t
 };
 
 namespace comma { namespace visiting {
+
+template <> struct traits< input_t >
+{
+    template < typename K, typename V > static void visit( const K&, input_t& p, V& v ) { v.apply( "t", p.time ); }
+    template < typename K, typename V > static void visit( const K&, const input_t& p, V& v ) { v.apply( "t", p.time ); }
+};
 
 template <> struct traits< output_t >
 {
@@ -142,16 +159,6 @@ int main( int ac, char** av )
         bool ignore_eof = options.exists( "--ignore-eof,--ignoreeof,--yes" );
         if( ignore_eof && !options.exists( "--period" ) ) { std::cerr << "csv-repeat: got --ignore-oef, thus please specify --period" << std::endl; return 1; }
         comma::csv::options csv = comma::csv::options( options );
-        std::size_t record_size = csv.binary() ? csv.format().size() : 0;
-        std::vector< char > buffer( csv.binary() ? ( 65536ul / record_size + 1 ) * record_size : 0 );
-        char* buffer_begin = &buffer[0];
-        const char* buffer_end = &buffer[0] + buffer.size();
-        char* read_position = buffer_begin;
-        char* write_position = buffer_begin;
-        char* last_record = NULL;
-        comma::io::select select;
-        select.read().add( comma::io::stdin_fd );
-        comma::io::istream is( "-", comma::io::mode::binary );
         boost::scoped_ptr< comma::csv::output_stream< output_t > > ostream;
         comma::csv::options output_csv;
         output_csv.full_xpath = false;
@@ -183,6 +190,12 @@ int main( int ac, char** av )
             output_csv.delimiter = csv.delimiter;
             ostream.reset( new comma::csv::output_stream< output_t >( std::cout, output_csv ) );
         }
+        boost::optional< boost::posix_time::time_duration > period;
+        if( options.exists( "--period" ) ) { period = boost::posix_time::microseconds( static_cast<unsigned int> (options.value< double >( "--period" ) * 1000000 )); }
+        boost::posix_time::time_duration timeout;
+        boost::optional< double > timeout_seconds = options.optional< double >( "--timeout,-t" );
+        if( !period && !timeout_seconds ) { std::cerr << "csv-repeat: please specify either --period, or --timeout, or both" << std::endl; return 1; }
+        timeout = timeout_seconds ? boost::posix_time::microseconds( static_cast<unsigned int>(*timeout_seconds * 1000000 )) : *period;
         if( options.exists( "--output-fields" ) )
         {
             if( !options.exists( "--fields,-f" ) ) { std::cerr << "csv-repeat: --output-fields option requires --fields" << std::endl; return 1; }
@@ -199,17 +212,73 @@ int main( int ac, char** av )
             std::cout << std::endl;
             return 0;
         }
-        boost::optional< boost::posix_time::time_duration > period;
-        if( options.exists( "--period" ) ) { period = boost::posix_time::microseconds( static_cast<unsigned int> (options.value< double >( "--period" ) * 1000000 )); }
-        boost::posix_time::time_duration timeout;
-        boost::optional< double > timeout_seconds = options.optional< double >( "--timeout,-t" );
-        if( !period && !timeout_seconds ) { std::cerr << "csv-repeat: please specify either --period, or --timeout, or both" << std::endl; return 1; }
-        timeout = timeout_seconds ? boost::posix_time::microseconds( static_cast<unsigned int>(*timeout_seconds * 1000000 )) : *period;
+        std::cin.tie( NULL );
+        if( options.exists( "--timestamped" ) )
+        {
+            if( !period ) { std::cerr << "csv-repeat: for --timestamped, please specify --period" << std::endl; return 1; }
+            if( options.exists( "--timeout,-t" ) ) { std::cerr << "csv-repeat: for --timestamped: --timeout not supported" << std::endl; return 1; }
+            comma::csv::input_stream< input_t > istream( std::cin, csv );
+            boost::posix_time::ptime last;
+            if( options.exists( "--from" ) ) { last = boost::posix_time::from_iso_string( options.value< std::string >( "--from" ) ); }
+            boost::posix_time::ptime to;
+            if( options.exists( "--to" ) ) { to = boost::posix_time::from_iso_string( options.value< std::string >( "--to" ) ); }
+            std::string last_record;
+            if( csv.binary() ) { last_record = std::string( csv.format().size(), 0 ); }
+            auto pass = [&]( const output_t& )
+            {
+                static comma::csv::passed< input_t > passed( istream, std::cout, csv.flush );
+                passed.write();
+            };
+            auto append = [&]( const output_t& o )
+            {
+                static comma::csv::tied< input_t, output_t > tied( istream, *ostream );
+                tied.append( o );
+            };
+            std::function< void( const output_t& p ) > write;
+            if( ostream ) { write = append; } else { write = pass; }
+            auto repeat = [&]( boost::posix_time::ptime now )
+            {
+                if( now.is_not_a_date_time() || last.is_not_a_date_time() ) { return; }
+                for( boost::posix_time::ptime t = last + *period; t < now; t += *period )
+                {
+                    std::cout.write( &last_record[0], last_record.size() );
+                    if( !csv.binary() ) { std::cout << csv.delimiter; }
+                    if( ostream ) { ostream->write( output_t( t, true ) ); }
+                }
+            };
+            auto set_last_record = [&]()
+            {
+                if( csv.binary() ) { std::memcpy( &last_record[0], istream.binary().last(), last_record.size() ); } // todo! quick and dirty, watch performance! we don't need to copy each record, but that would make the code more complex
+                else { last_record = comma::join( istream.ascii().last(), csv.delimiter ); }
+            };
+            while( istream.ready() || std::cin.good() )
+            {
+                const input_t* p = istream.read();
+                if( !p ) { break; }
+                if( p->time.is_not_a_date_time() ) { std::cerr << "csv-repeat: expected timestamp, got not a date/time" << std::endl; return 1; }
+                if( last_record.empty() && !last.is_not_a_date_time() ) { set_last_record(); } // quick and dirty
+                repeat( p->time );
+                write( output_t( p->time, false ) );
+                last = p->time;
+                set_last_record();
+            }
+            repeat( to );
+            return 0;
+        }
+        std::size_t record_size = csv.binary() ? csv.format().size() : 0;
+        std::vector< char > buffer( csv.binary() ? ( 65536ul / record_size + 1 ) * record_size : 0 );
+        char* buffer_begin = &buffer[0];
+        const char* buffer_end = &buffer[0] + buffer.size();
+        char* read_position = buffer_begin;
+        char* write_position = buffer_begin;
+        char* last_record = NULL;
+        comma::io::select select;
+        select.read().add( comma::io::stdin_fd );
+        comma::io::istream is( "-", comma::io::mode::binary );
         bool end_of_stream = false;
         std::string line;
         std::string last_line;
         std::ios_base::sync_with_stdio( false ); // unsync to make rdbuf()->in_avail() working
-        std::cin.tie( NULL ); // std::cin is tied to std::cout by default
         bool repeating = false;
         bool pace = options.exists( "--pace" );
         if( pace && !period ) { std::cerr << "csv-repeat: for --pace, please specify --period" << std::endl; return 1; }
