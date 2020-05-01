ccd7163ea85cb9146a8c6a206b716ae287360037
Vsevolod Vlaskine <vsevolod@abysssolutions.com.au>
2020-05-01 13:23:58 +1000

    csv-eval: installation moved from cmake to setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,6 +8,7 @@ setup(
         description         = 'comma python utilties',
         url                 = 'https://gitlab.com/orthographic/comma',
         license             = 'BSD 3-Clause',
+        scripts             =["comma/csv/applications/csv-eval"],
         packages            = [ 'comma', 'comma.csv', 'comma.csv.applications', 'comma.io', 'comma.numpy', 'comma.signal', 'comma.util', 'comma.cpp_bindings', 'comma.application' ],
         package_dir         = { 'comma.cpp_bindings': 'comma/cpp_bindings' },
         package_data        = { 'comma.cpp_bindings': [ '*.so', '*.dll' ] }
