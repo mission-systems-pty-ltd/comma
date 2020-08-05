7fdc12ec52118bfbbc59d30c25d9ec9532d7648f
Vsevolod Vlaskine <vsevolod@abysssolutions.com.au>
2020-08-05 13:00:02 +1000

    csv-shuffle: --binary: optimisation added that speeds it up 2-3 times for some types of input

--- csv/applications/csv-shuffle.cpp
+++ csv/applications/csv-shuffle.cpp
@@ -22,10 +22,6 @@ static void usage( bool verbose )
     std::cerr << "    --help,-h: help; --help --verbose: more help" << std::endl;
     std::cerr << "    --fields,-f,--input-fields <fields>: input fields" << std::endl;
     std::cerr << "    --output-fields,--output,-o <fields>: output fields" << std::endl;
-    std::cerr << "        semantics of outputting trailing fields:" << std::endl;
-    std::cerr << "            \"--output-fields=x,y\": do not output trailing fields" << std::endl;
-    std::cerr << "            \"--output-fields=x,y...\": output trailing fields" << std::endl;
-    std::cerr << "            see example below" << std::endl;
     std::cerr << "    --verbose,-v: more output" << std::endl;
     if( verbose ) { std::cerr << std::endl << comma::csv::options::usage() << std::endl; }
     std::cerr << std::endl;
@@ -40,10 +36,6 @@ static void usage( bool verbose )
     std::cerr << "            cat xyz.csv | csv-shuffle --fields=x,y,z --output-fields=y,z,x" << std::endl;
     std::cerr << "        remove x, swap y,z, append z two times:" << std::endl;
     std::cerr << "            cat xyz.csv | csv-shuffle --fields=x,y,z --output-fields=z,y,z,z" << std::endl;
-    std::cerr << "        do not output trailing fields: swap x and y, do not output z" << std::endl;
-    std::cerr << "            cat xyz.csv | csv-shuffle --fields=x,y --output-fields=y,x" << std::endl;
-    std::cerr << "        output trailing fields: swap x and y, output z" << std::endl;
-    std::cerr << "            cat xyz.csv | csv-shuffle --fields=x,y --output-fields=y,x,..." << std::endl;
     std::cerr << std::endl;
     exit( 0 );
 }
@@ -52,11 +44,10 @@ struct field
 {
     std::string name;
     unsigned int index;
-    unsigned int offset;
     boost::optional< unsigned int > input_index;
     unsigned int input_offset;
     unsigned int size;
-    field( const std::string& name, unsigned int index ) : name( name ), index( index ) {}
+    field( const std::string& name, unsigned int index ) : name( name ), index( index ), input_offset( 0 ), size( 0 ) {}
 };
 
 int main( int ac, char** av )
@@ -66,30 +57,43 @@ int main( int ac, char** av )
         comma::command_line_options options( ac, av, usage );
         comma::csv::options csv( options );
         csv.fields = options.value< std::string >( "--input-fields,--fields,-f", "" );
-        std::vector< std::string > input_fields = comma::split( csv.fields, ',' );
-        std::vector< std::string > output_fields = comma::split( options.value< std::string >( "--output-fields,--output,-o" ), ',' );
-        bool output_trailing_fields = output_fields.back() == "...";
-        if( output_fields.back() == "..." ) { output_fields.erase( output_fields.end() - 1 ); }
+        std::vector< std::string > input_fields = comma::split( csv.fields, ',', true );
+        std::vector< std::string > output_fields = comma::split( options.value< std::string >( "--output-fields,--output,-o" ), ',', true );
+        if( output_fields.empty() ) { std::cerr << "csv-shuffle: please specify at least one output field" << std::endl; return 1; }
+        if( output_fields.back() == "..." ) { std::cerr << "csv-shuffle: support for trailing fields has been removed; please specify input/output fields explicitly" << std::endl; return 1; }
         std::vector< field > fields;
-        for( unsigned int i = 0; i < output_fields.size(); ++i )
-        {
-            if( !output_fields[i].empty() ) { fields.push_back( field( output_fields[i], i ) ); }
-        }
+        unsigned size = 0;
         if( csv.binary() )
         {
-            for( unsigned int i = 0; i < input_fields.size(); ++i )
+            for( unsigned int i = 0; i < output_fields.size(); )
             {
-                for( unsigned int j = 0; j < fields.size(); ++j )
-                {
-                    if( fields[j].name != input_fields[i] ) { continue; }
-                    fields[j].input_index = i;
-                    fields[j].input_offset = csv.format().offset( i ).offset;
-                    fields[j].size = csv.format().offset( i ).size;
-                }
+                fields.push_back( field( output_fields[i], i ) );
+                unsigned int j = 0;
+                for( ; j < input_fields.size() && input_fields[j] != output_fields[i]; ++j );
+                if( j >= input_fields.size() ) { std::cerr << "csv-shuffle: output field '" << output_fields[i] << "' not found in input fields '" << csv.fields << "'" << std::endl; return 1; }
+                fields.back().input_offset = csv.format().offset( j ).offset;
+                for( ; i < output_fields.size() && j < input_fields.size() && input_fields[j] == output_fields[i]; ++i, ++j ) { fields.back().size += csv.format().offset( j ).size; }
+                size += fields.back().size;
+            }
+            //for( unsigned int i = 0; i < fields.size(); ++i ) { std::cerr << "--> i: " << i << " fields[i].name: " << fields[i].name << " fields[i].input_offset: " << fields[i].input_offset << " fields[i].size: " << fields[i].size << std::endl; }
+            #ifdef WIN32
+            _setmode( _fileno( stdin ), _O_BINARY );
+            _setmode( _fileno( stdout ), _O_BINARY );
+            #endif
+            std::vector< char > buf( csv.format().size() );
+            if( !csv.flush ) { std::cin.tie( NULL ); } // quick and dirty; std::cin is tied to std::cout by default, which is thread-unsafe now
+            while( std::cin.good() && !std::cin.eof() )
+            {
+                std::cin.read( &buf[0], csv.format().size() );
+                if( std::cin.gcount() == 0 ) { continue; }
+                if( std::cin.gcount() < int( csv.format().size() ) ) { std::cerr << "csv-shuffle: expected " << csv.format().size() << " bytes, got only " << std::cin.gcount() << std::endl; return 1; }
+                for( unsigned int i = 0; i < fields.size(); ++i ) { std::cout.write( &buf[ fields[i].input_offset ], fields[i].size ); }
+                if( csv.flush ) { std::cout.flush(); }
             }
         }
         else
         {
+            for( unsigned int i = 0; i < output_fields.size(); ++i ) { fields.push_back( field( output_fields[i], i ) ); }
             for( unsigned int i = 0; i < input_fields.size(); ++i )
             {
                 for( unsigned int j = 0; j < fields.size(); ++j )
@@ -97,97 +101,10 @@ int main( int ac, char** av )
                     if( fields[j].name == input_fields[i] ) { fields[j].input_index = i; }
                 }
             }
-        }
-        if( fields.empty() ) { std::cerr << "csv-shuffle: please define at least one output field" << std::endl; return 1; }
-        for( unsigned int i = 0; i < fields.size(); ++i )
-        {
-            if( !fields[i].input_index ) { std::cerr << "csv-shuffle: \"" << fields[i].name << "\" not found in input fields " << csv.fields << std::endl; return 1; }
-        }
-        if( csv.binary() )
-//         {
-//             #ifdef WIN32
-//             _setmode( _fileno( stdin ), _O_BINARY );
-//             _setmode( _fileno( stdout ), _O_BINARY );
-//             #endif
-//             std::vector< char > in( csv.format().size() );
-//             std::vector< comma::csv::format::element > elements;
-//             elements.reserve( csv.format().count() ); // quick and dirty, can be really wasteful on large things like images
-//             for( unsigned int i = 0; i < elements.capacity(); ++i )
-//             {
-//                 elements.push_back( csv.format().offset( i ) );
-//             }
-//             unsigned int previous_index = 0;
-//             unsigned int output_size = 0;
-//             for( unsigned int i = 0; i < fields.size(); ++i ) // quick and dirty
-//             {
-//                 for( unsigned int k = previous_index; k < fields[i].index && k < elements.size(); ++k ) { output_size += elements[k].size; }
-//                 output_size += fields[i].size;
-//                 previous_index = fields[i].index + 1;
-//             }
-//             for( unsigned int k = previous_index; output_trailing_fields && k < elements.size(); ++k ) { output_size += elements[k].size; }
-//             std::vector< char > out( output_size );
-//             if( !csv.flush && csv.binary() ) { std::cin.tie( NULL ); } // quick and dirty; std::cin is tied to std::cout by default, which is thread-unsafe now
-//             while( std::cin.good() && !std::cin.eof() )
-//             {
-//                 // todo: quick and dirty; if performance is an issue, you could read more than
-//                 // one record every time see comma::csv::binary_input_stream::read() for reference
-//                 std::cin.read( &in[0], csv.format().size() );
-//                 if( std::cin.gcount() == 0 ) { continue; }
-//                 if( std::cin.gcount() < int( csv.format().size() ) ) { std::cerr << "csv-shuffle: expected " << csv.format().size() << " bytes, got only " << std::cin.gcount() << std::endl; return 1; }
-//                 unsigned int offset = 0;
-//                 unsigned int previous_index = 0;
-//                 for( unsigned int i = 0; i < fields.size(); ++i ) // quick and dirty
-//                 {
-//                     for( unsigned int k = previous_index; k < fields[i].index && k < elements.size(); ++k )
-//                     {
-//                         std::memcpy( &out[offset], &in[ elements[k].offset ], elements[k].size ); // std::cout.write( &in[ elements[k].offset ], elements[k].size );
-//                         offset += elements[k].size;
-//                     }
-//                     std::memcpy( &out[offset], &in[ fields[i].input_offset ], fields[i].size ); // std::cout.write( &in[ fields[i].input_offset ], fields[i].size );
-//                     offset += fields[i].size;
-//                     previous_index = fields[i].index + 1;
-//                 }
-//                 //std::cerr << "--> previous_index: " << previous_index << " elements.size(): " << elements.size() << std::endl;
-//                 for( unsigned int k = previous_index; output_trailing_fields && k < elements.size(); ++k )
-//                 {
-//                     std::memcpy( &out[offset], &in[ elements[k].offset ], elements[k].size ); // std::cout.write( &in[ elements[k].offset ], elements[k].size );
-//                     offset += elements[k].size;
-//                 }
-//                 std::cout.write( &out[0], out.size() );
-//                 if( csv.flush ) { std::cout.flush(); }
-//             }
-//         }
-        {
-            #ifdef WIN32
-            _setmode( _fileno( stdin ), _O_BINARY );
-            _setmode( _fileno( stdout ), _O_BINARY );
-            #endif
-            std::vector< char > buf( csv.format().size() );
-            std::vector< comma::csv::format::element > elements;
-            elements.reserve( csv.format().count() ); // quick and dirty, can be really wasteful on large things like images
-            for( unsigned int i = 0; i < elements.capacity(); ++i ) { elements.push_back( csv.format().offset( i ) ); }
-            if( !csv.flush && csv.binary() ) { std::cin.tie( NULL ); } // quick and dirty; std::cin is tied to std::cout by default, which is thread-unsafe now
-            while( std::cin.good() && !std::cin.eof() )
+            for( unsigned int i = 0; i < fields.size(); ++i )
             {
-                // todo: quick and dirty; if performance is an issue, you could read more than
-                // one record every time see comma::csv::binary_input_stream::read() for reference
-                std::cin.read( &buf[0], csv.format().size() );
-                if( std::cin.gcount() == 0 ) { continue; }
-                if( std::cin.gcount() < int( csv.format().size() ) ) { std::cerr << "csv-shuffle: expected " << csv.format().size() << " bytes, got only " << std::cin.gcount() << std::endl; return 1; }
-                unsigned int previous_index = 0;
-                for( unsigned int i = 0; i < fields.size(); ++i ) // quick and dirty
-                {
-                    for( unsigned int k = previous_index; k < fields[i].index && k < elements.size(); ++k ) { std::cout.write( &buf[ elements[k].offset ], elements[k].size ); }
-                    std::cout.write( &buf[ fields[i].input_offset ], fields[i].size );
-                    previous_index = fields[i].index + 1;
-                }
-                //std::cerr << "--> previous_index: " << previous_index << " elements.size(): " << elements.size() << std::endl;
-                for( unsigned int k = previous_index; output_trailing_fields && k < elements.size(); ++k ) { std::cout.write( &buf[ elements[k].offset ], elements[k].size ); }
-                if( csv.flush ) { std::cout.flush(); }
+                if( !fields[i].input_index ) { std::cerr << "csv-shuffle: \"" << fields[i].name << "\" not found in input fields " << csv.fields << std::endl; return 1; }
             }
-        }
-        else
-        {
             while( std::cin.good() && !std::cin.eof() )
             {
                 std::string line;
@@ -207,11 +124,6 @@ int main( int ac, char** av )
                     previous_index = fields[i].index + 1;
                     std::cout << delimiter;
                     if ( *fields[i].input_index < v.size() ) { std::cout << v[ *fields[i].input_index ]; }
-                    delimiter = csv.delimiter;
-                }
-                for( unsigned int k = previous_index; output_trailing_fields && k < v.size(); ++k )
-                {
-                    std::cout << delimiter << v[k];
                     delimiter = csv.delimiter;
                 }
                 std::cout << std::endl;
