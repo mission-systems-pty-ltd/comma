6d665621f7c19bd89a069180618ed5482f6fe678
vlaskine <vsevolod.vlaskine@gmail.com>
2020-09-03 21:54:14 +1000

    csv-update: minor refactoring

--- csv/applications/csv-update.cpp
+++ csv/applications/csv-update.cpp
@@ -1,31 +1,4 @@
-// This file is part of comma, a generic and flexible library
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /// @author vsevolod vlaskine
 
@@ -41,7 +14,6 @@
 #include <boost/unordered_map.hpp>
 #include <boost/graph/graph_concepts.hpp>
 #include "../../application/command_line_options.h"
-#include "../../application/contact_info.h"
 #include "../../base/types.h"
 #include "../../csv/stream.h"
 #include "../../csv/impl/unstructured.h"
@@ -59,25 +31,11 @@ static void usage( bool more )
     std::cerr << std::endl;
     std::cerr << "       if update input specified (update.csv above), update" << std::endl;
     std::cerr << "       records from stdin by the ones in the update file" << std::endl;
-    if( more )
-    {
-        std::cerr << "       todo" << std::endl;
-    }
-    else
-    {
-        std::cerr << "       ... use --help --verbose for more" << std::endl;
-    }
+    if( more ) { std::cerr << "       todo" << std::endl; } else { std::cerr << "       ... use --help --verbose for more" << std::endl; }
     std::cerr << std::endl;
     std::cerr << "       if update input not specified, apply updates received" << std::endl;
     std::cerr << "       on stdin itself" << std::endl;
-    if( more )
-    {
-        std::cerr << "       todo" << std::endl;
-    }
-    else
-    {
-        std::cerr << "       ... use --help --verbose for more" << std::endl;
-    }
+    if( more ) { std::cerr << "       todo" << std::endl; } else { std::cerr << "       ... use --help --verbose for more" << std::endl; }
     std::cerr << std::endl;
     std::cerr << "    fields:" << std::endl;
     std::cerr << "        block: block number" << std::endl;
@@ -109,7 +67,7 @@ static void usage( bool more )
     std::cerr << "    --update-non-empty-fields,--update-non-empty,-u:" << std::endl;
     std::cerr << "        if update has empty fields, use the field value from stdin (for binary, empty fields must be defined with --empty)" << std::endl;
     std::cerr << "    --verbose,-v: more output to stderr" << std::endl;
-    if( more ) { std::cerr << std::endl << "csv options:" << std::endl << comma::csv::options::usage() << std::endl; }
+    if( more ) { std::cerr << std::endl << "csv options:" << std::endl << comma::csv::options::usage() << std::endl; } else { std::cerr << std::endl << "csv options ... use --help --verbose for more" << std::endl; }
     std::cerr << std::endl;
     if( more )
     {
@@ -153,8 +111,6 @@ static void usage( bool more )
     {
         std::cerr << "examples ... use --help --verbose" << std::endl;
     }
-    std::cerr << std::endl;
-    std::cerr << comma::contact_info << std::endl;
     std::cerr << std::endl;
     exit( 0 );
 }
@@ -244,8 +200,7 @@ static void output_and_clear( map_t::type& map, bool do_output, comma::csv::outp
         }
         for( output_map_t::const_iterator it = m.begin(); it != m.end(); ++it )
         {   
-            if( ostream ) { ostream->write( it->second->value, it->second->string ); }
-            else { std::cout << it->second->string; }
+            if( ostream ) { ostream->write( it->second->value, it->second->string ); } else { std::cout << it->second->string; }
         }
     }
     map.clear();
