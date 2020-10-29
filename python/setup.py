9b4cc26d6d19fcec0984676ec1e21838fdce3cc0
Vsevolod Vlaskine <vsevolod@abysssolutions.com.au>
2020-10-29 17:23:07 +1100

    python: packaging: installation version fixed

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import comma.version
 
 setup(
         name                = 'comma',
-        version             = open('comma/version.py').readlines()[-1].strip().split()[-1].strip('\"'),
+        version             = comma.version.__version__,
         description         = 'comma python utilties',
         url                 = 'https://gitlab.com/orthographic/comma',
         license             = 'BSD 3-Clause',
-        scripts             =["comma/csv/applications/csv-eval"],
+        scripts             = ["comma/csv/applications/csv-eval"],
         packages            = [ 'comma', 'comma.csv', 'comma.csv.applications', 'comma.io', 'comma.numpy', 'comma.signal', 'comma.util', 'comma.cpp_bindings', 'comma.application' ],
         package_dir         = { 'comma.cpp_bindings': 'comma/cpp_bindings' },
         package_data        = { 'comma.cpp_bindings': [ '*.so', '*.dll' ] }
