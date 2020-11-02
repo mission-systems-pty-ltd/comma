ae0014f5d3f4fcd56d35dfda1feb291987bb4623
vlaskine <vsevolod.vlaskine@gmail.com>
2020-11-02 11:43:34 +1100

    system/package: trying to make it work for ubuntu 18.04 with numpy 1.13...

--- python/comma/csv/time.py
+++ python/comma/csv/time.py
@@ -1,31 +1,4 @@
-# This file is part of comma, a generic and flexible library
 # Copyright (c) 2011 The University of Sydney
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University of Sydney nor the
-#    names of its contributors may be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-# GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-# HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import absolute_import
 import numpy as np
@@ -42,7 +15,7 @@ NOT_A_DATE_TIME = np.datetime64('NaT')
 POSITIVE_INFINITY = np.datetime64('294247-01-09T04:00:54.775807')
 NEGATIVE_INFINITY = np.datetime64('-290308-12-22T19:59:05.224191')
 BASESTRING = basestring if sys.version_info.major < 3 else str # sigh...
-NUMPY_VERSION_MAJOR_, NUMPY_VERSION_MINOR_, NUMPY_VERSION_PATH_ = ( int( _ ) for _ in np.__version__.split( '.' ) )
+NUMPY_VERSION_MAJOR_, NUMPY_VERSION_MINOR_, NUMPY_VERSION_PATCH_ = ( int( _ ) for _ in np.__version__.split( '.' ) )
 
 def is_undefined(numpy_time): return str(numpy_time) == str(NOT_A_DATE_TIME)
 
