04627cd40c7bf5e347bb58f662082377b65b867a
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-05-11 21:39:17 +1000

    comma/python: preparing for pypi

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import setuptools #from distutils.core import setup
+import setuptools, pathlib #from distutils.core import setup
 import comma.version
 
 setuptools.setup(
@@ -14,20 +14,22 @@ setuptools.setup(
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
-          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
           'Topic :: Communications :: Email'
         ],
-        description         = 'comma python utilities',
+        description         = 'csv and fixed-width binary python utilities',
         url                 = 'https://gitlab.com/orthographic/comma',
         license             = 'BSD 3-Clause',
-        long_description    = 'comma python utilities for offline and streamed csv and fixed width data',
+        long_description    = ( pathlib.Path(__file__).parent / "README.md" ).read_text(),
+        long_description_content_type = "text/markdown",
+        author              = "vsevolod vlaskine",
+        author_email        = "vsevolod.vlaskine@gmail.com",
         maintainer          = 'vsevolod vlaskine',
         maintainer_email    = 'vsevolod.vlaskine@gmail.com',
         python_requires     = '>=3.6',
-        install_requires    = ['numpy'], # todo?
+        install_requires    = [ 'numpy' ], # todo?
         packages            = [ 'comma', 'comma.csv', 'comma.csv.applications', 'comma.io', 'comma.numpy', 'comma.signal', 'comma.util', 'comma.cpp_bindings', 'comma.application' ],
         package_dir         = { 'comma': 'comma', 'comma.cpp_bindings': 'comma/cpp_bindings' },
         package_data        = { 'comma.cpp_bindings': [ '*.so', '*.dll' ] },
-        entry_points        = { 'console_scripts': ['csv-eval=comma.csv.applications.csv_eval:main'] },
-        #scripts             = [ "comma/csv/applications/csv-eval" ]
+        entry_points        = { 'console_scripts': ['csv-eval=comma.csv.applications.csv_eval:main'] } #scripts             = [ "comma/csv/applications/csv-eval" ]
      )
