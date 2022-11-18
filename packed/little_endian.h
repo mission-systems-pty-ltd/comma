c4d1d73d9322f67ede1b44bdc6e13d779e93e9b2
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-11-18 18:17:09 +1100

    packed: 6-byte little/big endian integers added; todo: unit test

--- packed/little_endian.h
+++ packed/little_endian.h
@@ -1,31 +1,4 @@
-// This file is part of comma, a generic and flexible library
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /// @author Matthew Herrmann 2007
 /// @author vsevolod vlaskine
@@ -48,6 +21,10 @@ typedef detail::endian< detail::little, 4, true > little_endian32;
 typedef detail::endian< detail::little, 4, false > little_endian_uint32;
 typedef little_endian32 int32;
 typedef little_endian_uint32 uint32;
+typedef detail::endian< detail::little, 6, true > little_endian48;
+typedef detail::endian< detail::little, 6, false > little_endian_uint48;
+typedef little_endian48 int48;
+typedef little_endian_uint48 uint48;
 typedef detail::endian< detail::little, 8, true > little_endian64;
 typedef detail::endian< detail::little, 8, false > little_endian_uint64;
 typedef little_endian64 int64;
@@ -66,6 +43,8 @@ typedef detail::endian< detail::little, 3, false > uint24;
 typedef detail::endian< detail::little, 3, true > int24;
 typedef detail::endian< detail::little, 4, false > uint32;
 typedef detail::endian< detail::little, 4, true > int32;
+typedef detail::endian< detail::little, 6, false > uint48;
+typedef detail::endian< detail::little, 6, true > int48;
 typedef detail::endian< detail::little, 8, false > uint64;
 typedef detail::endian< detail::little, 8, true > int64;
 typedef detail::endian< detail::little, 4, true, true > float32;
