00ff25827844972b5971b4f90e4bc7c977ff1ab3
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-11-23 12:32:27 +1100

    csv/names: fields_exist(): allow_empty flag added

--- csv/names.cpp
+++ csv/names.cpp
@@ -1,32 +1,4 @@
-// This file is part of comma, a generic and flexible library
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 
 /// @author vsevolod vlaskine
 
@@ -36,10 +8,11 @@
 
 namespace comma { namespace csv {
 
-bool fields_exist( const std::vector< std::string >& fields, const std::vector< std::string >& subset )
+bool fields_exist( const std::vector< std::string >& fields, const std::vector< std::string >& subset, bool allow_empty )
 {
     for( unsigned int i = 0; i < subset.size(); ++i )
     {
+        if( allow_empty && subset[i].empty() ) { continue; }
         bool found = false;
         for( unsigned int j = 0; !found && j < fields.size(); found = fields[j] == subset[i], ++j );
         if( !found ) { return false; }
@@ -47,9 +20,6 @@ bool fields_exist( const std::vector< std::string >& fields, const std::vector< 
     return true;
 }
 
-bool fields_exist( const std::string& fields, const std::string& subset, char delimiter )
-{
-    return fields_exist( comma::split( fields, delimiter ), comma::split( subset, delimiter ) );
-}
+bool fields_exist( const std::string& fields, const std::string& subset, char delimiter, bool allow_empty ) { return fields_exist( comma::split( fields, delimiter, true ), comma::split( subset, delimiter, true ), allow_empty ); }
 
 } } // namespace comma { namespace csv {
