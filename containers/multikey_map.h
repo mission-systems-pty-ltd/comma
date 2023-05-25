1e9e9085002a2fc78c62248e62f72cde1fc7df75
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-05-25 18:44:16 +1000

    comma::multikey_map::map_type added

--- containers/multikey_map.h
+++ containers/multikey_map.h
@@ -41,6 +41,8 @@ class multikey_map : public std::unordered_map< std::array< comma::int32, Size >
 {
     public:
         typedef std::unordered_map< std::array< comma::int32, Size >, V, comma::array_hash< std::array< comma::int32, Size >, Size > > base_type;
+
+        typedef base_type map_type;
         
         enum { dimensions = Size };
         
