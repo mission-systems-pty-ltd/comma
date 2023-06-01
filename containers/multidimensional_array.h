2ec611dfcfdd56db078e3823f92dde283e908666
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-06-01 14:00:07 +1000

    containers: multidimensional_array: storage type added

--- containers/multidimensional_array.h
+++ containers/multidimensional_array.h
@@ -10,13 +10,15 @@
 
 namespace comma {
 
-template < typename V, unsigned int D >
+template < typename V, unsigned int D, typename S = std::vector< V > >
 class multidimensional_array
 {
     public:
         typedef std::array< std::size_t, D > index_type;
 
         typedef V value_type;
+
+        typedef S storage_type;
 
         const unsigned int dimensions{D};
 
@@ -26,7 +28,7 @@ class multidimensional_array
 
         const V& operator()( const index_type& i ) const { return _data[ _index( i ) ]; }
 
-        const std::vector< V >& data() const { return _data; }
+        const storage_type& data() const { return _data; }
 
         const index_type& size() const { return _size; }
 
@@ -46,11 +48,11 @@ class multidimensional_array
                 bool operator!=( const const_iterator& rhs ) const { return !operator==( rhs ); }
 
             private:
-                friend class multidimensional_array< V, D >;
+                friend class multidimensional_array< V, D, S >;
                 std::size_t _i{0};
-                typename std::vector< V >::iterator _it;
+                typename storage_type::iterator _it;
                 index_type _size;
-                iterator( std::size_t i, typename std::vector< V >::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
+                iterator( std::size_t i, typename storage_type::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
         };
 
         class const_iterator
@@ -66,11 +68,11 @@ class multidimensional_array
                 bool operator!=( const const_iterator& rhs ) const { return !operator==( rhs ); }
 
             private:
-                friend class multidimensional_array< V, D >;
+                friend class multidimensional_array< V, D, S >;
                 std::size_t _i{0};
-                typename std::vector< V >::iterator _it;
+                typename storage_type::iterator _it;
                 index_type _size;
-                const_iterator( std::size_t i, typename std::vector< V >::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
+                const_iterator( std::size_t i, typename storage_type::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
         };
 
         iterator begin() { return iterator( 0, _data.begin(), _size ); }
@@ -83,18 +85,20 @@ class multidimensional_array
 
     private:
         index_type _size;
-        std::vector< V > _data;
+        storage_type _data;
         std::size_t _index( const index_type& i );
         std::size_t _product( const index_type& i );
 };
 
-template < typename V, unsigned int D, typename P = std::array< double, D > >
-class interpolated_multidimensional_array: public multidimensional_array< V, D >
+// todo: slice
+
+template < typename V, unsigned int D, typename P = std::array< double, D >, typename S = std::vector< V > >
+class interpolated_multidimensional_array: public multidimensional_array< V, D, S >
 {
     public:
         typedef P point_type;
 
-        typedef multidimensional_array< V, D > base_type;
+        typedef multidimensional_array< V, D, S > base_type;
 
         typedef typename base_type::index_type index_type;
 
@@ -102,7 +106,11 @@ class interpolated_multidimensional_array: public multidimensional_array< V, D >
 
         interpolated_multidimensional_array( const P& origin, const P& resolution, const index_type& size, const V& default_value = V() );
 
-        index_type index_of( const point_type& p ) const;
+        index_type index_of( const point_type& p ) const; // p: 1.,2.,3. -> return: 23,22,21
+
+        /// q = index_as_point( p ); // p: 1.,2.,3. -> 21.,22.,23
+        /// index_of( p ) == index_of( origin + q * resolution );
+        point_type index_as_point( const point_type& p ) const; // p: 1.,2.,3. -> 21.,22.,23
 
         V& operator()( const point_type& p ) { return operator()( index_of( p ) ); }
 
@@ -140,16 +148,16 @@ struct index< D, 1 >
 
 } // namespace impl {
 
-template < typename V, unsigned int D >
-inline std::size_t multidimensional_array< V, D >::_index( const typename multidimensional_array< V, D >::index_type& i ) { return impl::index< D >::value( i, _size ); }
+template < typename V, unsigned int D, typename S >
+inline std::size_t multidimensional_array< V, D, S >::_index( const typename multidimensional_array< V, D, S >::index_type& i ) { return impl::index< D >::value( i, _size ); }
 
-template < typename V, unsigned int D >
-inline std::size_t multidimensional_array< V, D >::_product( const typename multidimensional_array< V, D >::index_type& i ) { return impl::index< D >::product( i ); }
+template < typename V, unsigned int D, typename S >
+inline std::size_t multidimensional_array< V, D, S >::_product( const typename multidimensional_array< V, D, S >::index_type& i ) { return impl::index< D >::product( i ); }
 
-template < typename V, unsigned int D >
-inline typename multidimensional_array< V, D >::index_type multidimensional_array< V, D >::iterator::index() const { return impl::index< D >::value( _i, _size ); }
+template < typename V, unsigned int D, typename S >
+inline typename multidimensional_array< V, D, S >::index_type multidimensional_array< V, D, S >::iterator::index() const { return impl::index< D >::value( _i, _size ); }
 
-template < typename V, unsigned int D >
-inline typename multidimensional_array< V, D >::index_type multidimensional_array< V, D >::const_iterator::index() const { return impl::index< D >::value( _i, _size ); }
+template < typename V, unsigned int D, typename S >
+inline typename multidimensional_array< V, D, S >::index_type multidimensional_array< V, D, S >::const_iterator::index() const { return impl::index< D >::value( _i, _size ); }
 
 } // namespace comma {
