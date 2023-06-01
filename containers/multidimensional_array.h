c71812c43a0b3888bb26d08e70d7bad1f8eb820b
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-06-01 12:05:04 +1000

    containers: multidimensional_array: a couple of bugs fixed; unit-testing: basics done

--- containers/multidimensional_array.h
+++ containers/multidimensional_array.h
@@ -20,7 +20,7 @@ class multidimensional_array
 
         const unsigned int dimensions{D};
 
-        multidimensional_array( const index_type& size, const V& default_value = V() ): _size( size ), _data( _index( _size ), default_value ) {}
+        multidimensional_array( const index_type& size, const V& default_value = V() ): _size( size ), _data( _product( _size ), default_value ) {}
 
         V& operator()( const index_type& i ) { return _data[ _index( i ) ]; }
 
@@ -36,11 +36,10 @@ class multidimensional_array
         {
             public:
                 iterator() = default;
-                std::pair< index_type, V& >&& operator->();
-                std::pair< index_type, const V& >&& operator->() const;
                 V& operator*() { return *_it; }
                 const V& operator*() const { return *_it; }
                 iterator& operator++() { ++_it; ++_i; return *this; }
+                index_type index() const;
                 bool operator==( const iterator& rhs ) const { return _it == rhs._it; }
                 bool operator==( const const_iterator& rhs ) const { return _it == rhs._it; }
                 bool operator!=( const iterator& rhs ) const { return !operator==( rhs ); }
@@ -50,16 +49,17 @@ class multidimensional_array
                 friend class multidimensional_array< V, D >;
                 std::size_t _i{0};
                 typename std::vector< V >::iterator _it;
-                iterator( std::size_t i, typename std::vector< V >::iterator it ): _i( i ), _it( it ) {}
+                index_type _size;
+                iterator( std::size_t i, typename std::vector< V >::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
         };
 
         class const_iterator
         {
             public:
                 const_iterator() = default;
-                std::pair< index_type, const V& >&& operator->() const;
                 const V& operator*() const { return *_it; }
                 const_iterator& operator++() { ++_it; ++_i; return *this; }
+                index_type index() const;
                 bool operator==( const iterator& rhs ) const { return _it == rhs._it; }
                 bool operator==( const const_iterator& rhs ) const { return _it == rhs._it; }
                 bool operator!=( const iterator& rhs ) const { return !operator==( rhs ); }
@@ -69,21 +69,23 @@ class multidimensional_array
                 friend class multidimensional_array< V, D >;
                 std::size_t _i{0};
                 typename std::vector< V >::iterator _it;
-                const_iterator( std::size_t i, typename std::vector< V >::iterator it ): _i( i ), _it( it ) {}
+                index_type _size;
+                const_iterator( std::size_t i, typename std::vector< V >::iterator it, const index_type& size ): _i( i ), _it( it ), _size( size ) {}
         };
 
-        iterator begin() { return iterator( 0, _data.begin() ); }
+        iterator begin() { return iterator( 0, _data.begin(), _size ); }
 
-        const_iterator begin() const { return const_iterator( 0, _data.begin() ); }
+        const_iterator begin() const { return const_iterator( 0, _data.begin(), _size ); }
 
-        iterator end() { return iterator( _data.size(), _data.end() ); }
+        iterator end() { return iterator( _data.size(), _data.end(), _size ); }
 
-        const_iterator end() const { return const_iterator( _data.size(), _data.end() ); }
+        const_iterator end() const { return const_iterator( _data.size(), _data.end(), _size ); }
 
     private:
         index_type _size;
         std::vector< V > _data;
         std::size_t _index( const index_type& i );
+        std::size_t _product( const index_type& i );
 };
 
 
@@ -97,6 +99,7 @@ struct index
     static unsigned int value( const index_type& i, const index_type& size ) { return i[ I - 1 ] + index< D, I - 1 >::value( i, size ) * size[ I - 1 ]; }
     static void value( std::size_t j, index_type& i, const index_type& size ) { i[ I - 1 ] = j % size[ I - 1 ]; index< D, I - 1 >::value( j / size[ I - 1 ], i, size ); }
     static index_type value( std::size_t j, const index_type& size ) { index_type i; value( j, i, size ); return i; }
+    static std::size_t product( const index_type& i ) { return i[ I - 1 ] * index< D, I - 1 >::product( i ); }
 };
 
 template < unsigned int D >
@@ -105,6 +108,7 @@ struct index< D, 1 >
     typedef std::array< std::size_t, D > index_type;
     static unsigned int value( const index_type& i, const index_type& ) { return i[0]; }
     static void value( std::size_t j, index_type& i, const index_type& size ) { i[0] = j; }
+    static std::size_t product( const index_type& i ) { return i[0]; }
 };
 
 } // namespace impl {
@@ -113,12 +117,12 @@ template < typename V, unsigned int D >
 inline std::size_t multidimensional_array< V, D >::_index( const typename multidimensional_array< V, D >::index_type& i ) { return impl::index< D >::value( i, _size ); }
 
 template < typename V, unsigned int D >
-std::pair< typename multidimensional_array< V, D >::index_type, V& >&& multidimensional_array< V, D >::iterator::operator->() { return std::make_pair( impl::index< D >::value( _i, _size ), *_it ); }
+inline std::size_t multidimensional_array< V, D >::_product( const typename multidimensional_array< V, D >::index_type& i ) { return impl::index< D >::product( i ); }
 
 template < typename V, unsigned int D >
-std::pair< typename multidimensional_array< V, D >::index_type, const V& >&& multidimensional_array< V, D >::iterator::operator->() const { return std::make_pair( impl::index< D >::value( _i, _size ), *_it ); }
+inline typename multidimensional_array< V, D >::index_type multidimensional_array< V, D >::iterator::index() const { return impl::index< D >::value( _i, _size ); }
 
 template < typename V, unsigned int D >
-std::pair< typename multidimensional_array< V, D >::index_type, const V& >&& multidimensional_array< V, D >::const_iterator::operator->() const { return std::make_pair( impl::index< D >::value( _i, _size ), *_it ); }
+inline typename multidimensional_array< V, D >::index_type multidimensional_array< V, D >::const_iterator::index() const { return impl::index< D >::value( _i, _size ); }
 
 } // namespace comma {
--- containers/test/multidimensional_array_test.cpp
+++ containers/test/multidimensional_array_test.cpp
@@ -3,11 +3,6 @@
 #include <gtest/gtest.h>
 #include "../multidimensional_array.h"
 
-TEST( multidimensional_array, usage )
-{
-    // todo
-}
-
 TEST( multidimensional_array, impl_index_value )
 {
     EXPECT_EQ( comma::impl::index< 2 >::value( {0, 0}, {1, 5} ), 0 );
@@ -21,7 +16,17 @@ TEST( multidimensional_array, impl_index_value )
     EXPECT_EQ( comma::impl::index< 3 >::value( {0, 0, 0}, {2, 3, 4} ), 0 );
     EXPECT_EQ( comma::impl::index< 3 >::value( {0, 0, 3}, {2, 3, 4} ), 3 );
     EXPECT_EQ( comma::impl::index< 3 >::value( {0, 1, 3}, {2, 3, 4} ), 7 );
-    EXPECT_EQ( comma::impl::index< 3 >::value( {2, 1, 3}, {2, 3, 4} ), 3 + 4 * ( 1 + 3 * 2 ) );
+    EXPECT_EQ( comma::impl::index< 3 >::value( {1, 2, 3}, {2, 3, 4} ), 3 + 4 * ( 2 + 3 * 1 ) );
+    EXPECT_EQ( comma::impl::index< 3 >::value( {1, 2, 4}, {2, 3, 4} ), 2 * 3 * 4 );
+    EXPECT_EQ( comma::impl::index< 3 >::product( {2, 3, 4} ), 2 * 3 * 4 );
+}
+
+TEST( multidimensional_array, impl_index_product )
+{
+    EXPECT_EQ( comma::impl::index< 1 >::product( {2} ), 2 );
+    EXPECT_EQ( comma::impl::index< 2 >::product( {2, 3} ), 6 );
+    EXPECT_EQ( comma::impl::index< 3 >::product( {2, 3, 4} ), 2 * 3 * 4 );
+    EXPECT_EQ( comma::impl::index< 4 >::product( {2, 3, 4, 5} ), 2 * 3 * 4 * 5 );
 }
 
 TEST( multidimensional_array, impl_index_inverted_value )
@@ -33,13 +38,54 @@ TEST( multidimensional_array, impl_index_inverted_value )
     // todo: way more tests
 }
 
-TEST( multidimensional_array, basics )
+TEST( multidimensional_array, iteration )
+{
+    {
+        typedef std::array< std::size_t, 3 > array_t;
+        comma::multidimensional_array< int, 3 > a( {2, 3, 4}, 0 );
+        { array_t s{2, 3, 4}; EXPECT_EQ( a.size(), s ); EXPECT_EQ( a.data().size(), 2 * 3 * 4 ); }
+        unsigned int i = 0;
+        for( auto it = a.begin(); it != a.end(); ++it ) { *it = i++; }
+        auto it = a.begin();
+        { array_t a{0, 0, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 0 ); ++it; }
+        { array_t a{0, 0, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 1 ); ++it; }
+        { array_t a{0, 0, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 2 ); ++it; }
+        { array_t a{0, 0, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 3 ); ++it; }
+        { array_t a{0, 1, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 4 ); ++it; }
+        { array_t a{0, 1, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 5 ); ++it; }
+        { array_t a{0, 1, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 6 ); ++it; }
+        { array_t a{0, 1, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 7 ); ++it; }
+        { array_t a{0, 2, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 8 ); ++it; }
+        { array_t a{0, 2, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 9 ); ++it; }
+        { array_t a{0, 2, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 10 ); ++it; }
+        { array_t a{0, 2, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 11 ); ++it; }
+        { array_t a{1, 0, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 12 ); ++it; }
+        { array_t a{1, 0, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 13 ); ++it; }
+        { array_t a{1, 0, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 14 ); ++it; }
+        { array_t a{1, 0, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 15 ); ++it; }
+        { array_t a{1, 1, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 16 ); ++it; }
+        { array_t a{1, 1, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 17 ); ++it; }
+        { array_t a{1, 1, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 18 ); ++it; }
+        { array_t a{1, 1, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 19 ); ++it; }
+        { array_t a{1, 2, 0}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 20 ); ++it; }
+        { array_t a{1, 2, 1}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 21 ); ++it; }
+        { array_t a{1, 2, 2}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 22 ); ++it; }
+        { array_t a{1, 2, 3}; EXPECT_EQ( it.index(), a ); EXPECT_EQ( *it, 23 ); ++it; }
+        EXPECT_TRUE( it == a.end() );
+    }
+}
+
+TEST( multidimensional_array, access )
 {
     {
         comma::multidimensional_array< int, 3 > a( {2, 3, 4}, 0 );
         unsigned int i = 0;
         for( auto it = a.begin(); it != a.end(); ++it ) { *it = i++; }
-        EXPECT_EQ( a( {2, 1, 3} ), 3 + 4 * ( 1 + 3 * 2 ) );
-        // todo: more tests
+        EXPECT_EQ( a( {0, 0, 0} ), 0 );
+        EXPECT_EQ( a( {0, 1, 0} ), 4 );
+        EXPECT_EQ( a( {1, 2, 3} ), 23 );
+        a( {0, 0, 0} ) = 111; EXPECT_EQ( a( {0, 0, 0} ), 111 );
+        a( {1, 1, 2} ) = 222; EXPECT_EQ( a( {1, 1, 2} ), 222 );
+        a( {1, 2, 3} ) = 333; EXPECT_EQ( a( {1, 2, 3} ), 333 );
     }
 }
