3058703f391e10f9c49fe49f3e8a30c59b5ed97b
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-12-15 15:14:03 +1100

    timing: typo fixed

--- timing/duration.cpp
+++ timing/duration.cpp
@@ -11,7 +11,7 @@ boost::posix_time::time_duration from_seconds( double seconds )
 {
     long long s = seconds;
     int microseconds = ::ceil( ( seconds - s ) * 1000000 - 0.5 ); //int microseconds = ::round( ( d - seconds ) * 1000000 ); // although ::round() is slow, have to round, since lexical cast has floating point jitter, e.g. try: boost::lexical_cast< double >( "1369179610.752231000" );
-    return boost::posix_time::time_duration( boost::posix_time::seconds( seconds ) + boost::posix_time::microseconds( microseconds ) );
+    return boost::posix_time::time_duration( boost::posix_time::seconds( s ) + boost::posix_time::microseconds( microseconds ) );
 }
 
 } } } // namespace comma { namespace timing { namespace duration {
