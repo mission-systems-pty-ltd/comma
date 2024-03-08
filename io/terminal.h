08f801c62eb867d54511eec2014102abd45aa8fd
seva <seva@tinbox>
2024-03-08 18:36:07 +1100

    io::terminal::ostream: first cut implemented

---
+++ io/terminal.h
@@ -0,0 +1,74 @@
+// Copyright (c) 2024 Vsevolod Vlaskine
+
+/// @author vsevolod vlaskine
+
+#pragma once
+
+#include <iostream>
+
+namespace comma { namespace io { namespace terminal {
+
+namespace controls {
+
+struct end { static constexpr char value[] = { 0x07, 0 }; };
+
+struct stderr { static constexpr char* start = ""; static constexpr char* end = "\n"; }; // quick and dirty
+
+struct titlebar { static constexpr char start[] = { 0x1b, ']', '0', ';', 0 }; static constexpr char end[] = { 0x07, 0 }; };
+
+template < unsigned int Colour >
+struct coloured; // todo
+
+// todo! combining multiple controls
+
+} // namespace controls {
+
+template < typename T >
+void write_to( const std::string& s ) { std::cerr << T::start << s << T::end; }
+
+namespace impl {
+
+template < typename S > struct traits
+{
+    template < typename T > static bool output_if_end( bool started ) { return false; }
+    static void output( const S& s ) { std::cerr << s; }
+};
+
+template <> struct traits< terminal::controls::end >
+{
+    template < typename T > static bool output_if_end( bool started ) { if( started ) { std::cerr << T::end; } return true; }
+    static void output( const terminal::controls::end& s ) {}
+};
+
+} // namespace impl {
+
+template < typename T >
+class ostream
+{
+    public:
+        ~ostream() { if( _started ) { std::cerr << T::end; _started = false; } }
+
+        static terminal::controls::end end() { return terminal::controls::end(); } // convenience method
+
+        template < typename S >
+        ostream& operator<<( const S& s )
+        {
+            if( impl::traits< S >::template output_if_end< T >( _started ) )
+            {
+                _started = false;
+            }
+            else
+            {
+                if( !_started ) { std::cerr << T::start; _started = true; }
+                impl::traits< S >::output( s );
+            }
+            return *this;
+        }
+
+    protected:
+        bool _started{false};
+};
+
+typedef ostream< controls::titlebar > titlebar_ostream;
+
+} } } // namespace comma { namespace io { namespace terminal {
