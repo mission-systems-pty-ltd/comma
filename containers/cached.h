0ab47a0c164963274960f43722681c9477567008
seva <seva@tinbox>
2024-11-08 11:41:50 +1100

    containers: cached: beefed up; still, usage is clunky...

--- containers/cached.h
+++ containers/cached.h
@@ -1,32 +1,53 @@
 // Copyright (c) 2024 Vsevolod Vlaskine
 // All Rights Reserved
 
+#include <deque>
 #include <unordered_map>
 
 namespace comma {
 
-template < typename T, typename K >
+template < typename T, typename K, typename Hash = std::hash< K > >
 class cached
 {
     public:
+        cached( unsigned int max_size = 0 ): _size( max_size ) {}
+
         template < typename... Args >
         T& operator()( Args... args );
 
-        void clear() { _cache.clear(); }
+        void clear() { _values.clear(); }
 
-        const std::unordered_map< K, T >& cache() const { return _cache; }
+        void pop( unsigned int size = 1 );
+
+        const std::unordered_map< K, T, Hash >& values() const { return _values; }
 
     private:
-        std::unordered_map< K, T > _cache;
+        std::unordered_map< K, T, Hash > _values;
+        std::deque< K > _keys;
+        unsigned int _size{0};
 };
 
-template < typename T, typename K >
+template < typename T, typename K, typename Hash >
 template < typename... Args >
-T& cached< T, K >::operator()( Args... args )
+T& cached< T, K, Hash >::operator()( Args... args )
 {
     K k{ args... };
-    auto i = _cache.find( k );
-    return ( i == _cache.end() ? _cache.insert_emplace( k, T( k ) ).first : i )->second;
+    auto i = _values.find( k );
+    if( i != _values.end() ) { return i->second; }
+    if( _size > 0 && _values.size() == _size ) { pop(); }
+    _keys.emplace_back( k );
+    return _values.emplace( std::make_pair( k, T( k ) ) ).first->second;
+}
+
+template < typename T, typename K, typename Hash >
+inline void cached< T, K, Hash >::pop( unsigned int size )
+{
+    for( unsigned int i = 0; i < size; ++i )
+    {
+        if( _keys.empty() ) { return; }
+        _values.erase( _keys.front() );
+        _keys.pop_front();
+    }
 }
 
 } // namespace comma {
--- containers/test/cached_test.cpp
+++ containers/test/cached_test.cpp
@@ -2,9 +2,82 @@
 // All Rights Reserved
 
 #include <gtest/gtest.h>
+#include <boost/functional/hash.hpp>
 #include "../cached.h"
+
+struct square
+{
+    typedef int key;
+
+    square( int x ): a( x ) {}
+
+    int calculate( int x ) const { return a * x; }
+
+    int a{0};
+};
 
 TEST( cached, basics )
 {
-    // todo
+    comma::cached< square, int > c;
+    EXPECT_EQ( c( 5 ).calculate( 5 ), 25 );
+    EXPECT_EQ( c.values().size(), 1 );
+    EXPECT_EQ( c( 5 ).calculate( 5 ), 25 );
+    EXPECT_EQ( c.values().size(), 1 );
+    EXPECT_EQ( c( 10 ).calculate( 10 ), 100 );
+    EXPECT_EQ( c.values().size(), 2 );
+    EXPECT_EQ( c( 10 ).calculate( 10 ), 100 );
+    EXPECT_EQ( c.values().size(), 2 );
+    EXPECT_EQ( c( 20 ).calculate( 20 ), 400 );
+    EXPECT_EQ( c.values().size(), 3 );
+    EXPECT_EQ( c( 20 ).calculate( 20 ), 400 );
+    EXPECT_EQ( c.values().size(), 3 );
+    c.pop();
+    EXPECT_EQ( c.values().size(), 2 );
+    EXPECT_EQ( c.values().size(), 2 );
+    EXPECT_EQ( c( 20 ).calculate( 20 ), 400 );
+    EXPECT_EQ( c.values().size(), 2 );
+}
+
+struct someclass
+{
+    typedef std::pair< int, int > key;
+
+    struct hash
+    {
+        std::size_t operator()( key const& p ) const
+        {
+            std::size_t seed = 0;
+            boost::hash_combine( seed, p.first );
+            boost::hash_combine( seed, p.second );
+            return seed;
+        }
+    };
+
+    someclass( const key& k ) {}
+
+    void dummy( int x, int y ) const {}
+};
+
+// struct pair_hash : public std::unary_function< Array, std::size_t >
+// {
+//     std::size_t operator()( Array const& array ) const
+//     {
+//         std::size_t seed = 0;
+//         for( std::size_t i = 0; i < Size; ++i ) { boost::hash_combine( seed, array[i] ); }
+//         return seed;
+//         // return boost::hash_range( &array[0], &array[Size] ); // not so easy...
+//     }
+// };
+
+TEST( cached, key )
+{
+    comma::cached< someclass, someclass::key, someclass::hash > c;
+    c( 1, 2 ).dummy( 1, 2 );
+    EXPECT_EQ( c.values().size(), 1 );
+    c( 1, 2 ).dummy( 1, 2 );
+    EXPECT_EQ( c.values().size(), 1 );
+    c( 3, 1 ).dummy( 3, 1 );
+    EXPECT_EQ( c.values().size(), 2 );
+    c( 3, 1 ).dummy( 3, 1 );
+    EXPECT_EQ( c.values().size(), 2 );
 }
